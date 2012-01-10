dbce55d35fe9e97b7a89ef798d0190fb8c0df089
Stephen Delano <stephen@opscode.com>
2012-01-10 15:23:35 -0800

    add gdbm software config

---
+++ config/software/gdbm.rb
@@ -0,0 +1,15 @@
+name "gdbm"
+
+dependencies ["autoconf"]
+
+source :url => "http://ftp.gnu.org/gnu/gdbm/gdbm-1.9.1.tar.gz",
+       :md5 => "59f6e4c4193cb875964ffbe8aa384b58"
+
+relative_path "gdbm-1.9.1"
+
+build do
+  command "/opt/opscode/embedded/bin/autoconf"
+  command "./configure --prefix=/opt/opscode/embedded"
+  command "make BINOWN=root BINGRP=wheel" # TODO: is this a leftover from os x, a bug?
+  command "make install"
+end
