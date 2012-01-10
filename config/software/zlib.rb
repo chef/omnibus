3078f71b156ea9f14da6d5011a441b95cdcb68c8
Stephen Delano <stephen@opscode.com>
2012-01-10 15:25:56 -0800

    update zlib software config

--- config/software/zlib.rb
+++ config/software/zlib.rb
@@ -1,17 +1,27 @@
 name "zlib"
-description "teh zlibz"
 dependencies []
+
 source :url => "http://zlib.net/zlib-1.2.5.tar.gz",
        :md5 => "c735eab2d659a96e5a594c9e8541ad63"
+
 relative_path "zlib-1.2.5"
 
-# TODO: make this platform-independent, below is only for darwin
-configure_env = {
-  "LDFLAGS" => "-R/opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include",
-  "CFLAGS" => "-I/opt/opscode/embedded/include -L/opt/opscode/embedded/lib"
-}
+configure_env =
+  case platform
+  when "darwin"
+    {
+      "LDFLAGS" => "-R/opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include",
+      "CFLAGS" => "-I/opt/opscode/embedded/include -L/opt/opscode/embedded/lib"
+    }
+  else
+    {
+      "LDFLAGS" => "-Wl,-rpath /opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include",
+      "CFLAGS" => "-I/opt/opscode/embedded/include -L/opt/opscode/embedded/lib"
+    }
+  end
 
 build do
+  command "mkdir -p /opt/opscode/embedded/lib" # zlib's make needs this for some reason
   command "./configure --prefix=/opt/opscode/embedded", :env => configure_env
   command "make"
   command "make install"
