d439443e8d48d8227c611825cdb3a8986404f629
Stephen Delano <stephen@opscode.com>
2012-01-10 14:41:30 -0800

    add help2man software config

---
+++ config/software/help2man.rb
@@ -0,0 +1,22 @@
+name "help2man"
+
+dependencies []
+
+source :url => "http://ftp.gnu.org/gnu/help2man/help2man-1.40.5.tar.gz",
+       :md5 => "75a7d2f93765cd367aab98986a75f88c"
+
+relative_path "help2man-1.40.5"
+
+make_command =
+  case platform
+  when "solaris2", "freebsd"
+    "qmake"
+  else
+    "make"
+  end
+
+build do
+  command "./configure --prefix=/opt/opscode/embedded"
+  command make_command
+  command "#{make_command} install"
+end
