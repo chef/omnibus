2bfbfba71133cc44f1b64203e78339cba5cf8cc7
Stephen Delano <stephen@opscode.com>
2012-01-10 16:52:12 -0800

    add ncurses software config

---
+++ config/software/ncurses.rb
@@ -0,0 +1,14 @@
+name "ncurses"
+
+source :url => "http://ftp.gnu.org/gnu/ncurses/ncurses-5.9.tar.gz",
+       :md5 => "8cb9c412e5f2d96bc6f459aa8c6282a1"
+
+relative_path "ncurses-5.9"
+
+env = {"LD_RUN_PATH" => "/opt/opscode/embedded/lib"}
+
+build do
+  command "./configure --prefix=/opt/opscode/embedded --with-shared --without-debug", :env => env
+  command "make", :env => env
+  command "make install", :env => env
+end
