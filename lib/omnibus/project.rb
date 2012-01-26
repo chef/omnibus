2c87860574c81e48f415127765c53799bd8442ef
Stephen Delano <stephen@opscode.com>
2012-01-26 14:54:58 -0800

    basic project dsl

--- lib/omnibus/project.rb
+++ lib/omnibus/project.rb
@@ -4,5 +4,51 @@
 module Omnibus
   class Project
     include Rake::DSL
+
+    attr_reader :name
+    attr_reader :description
+    attr_reader :dependencies
+
+    def initialize(io)
+      instance_eval(io)
+      render_tasks
+    end
+
+    def name(val)
+      @name = val
+    end
+
+    def description(val)
+      @description = val
+    end
+
+    def dependencies(val)
+      @dependencies = val
+    end
+
+    private
+
+    def render_tasks
+      namespace :projects do
+        desc "package #{@name}"
+        task @name => (@dependencies.map {|dep| "software:#{dep}"}) do
+          command = ["fpm",
+                     "-s dir",
+                     "-t deb",
+                     "-v 0.0.1",
+                     "-n #{@name}",
+                     "/opt/opscode",
+                     "-m 'Opscode, Inc.'",
+                     "--description 'The full stack of #{@name}'",
+                     "--url http://www.opscode.com"].join(" ")
+          shell = Mixlib::ShellOut.new(command,
+                                       :live_stream => STDOUT,
+                                       :timeout => 3600,
+                                       :cwd => './pkg')
+          shell.run_command
+          shell.error!
+        end
+      end
+    end
   end
 end
