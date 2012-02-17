7c42e31cac8a4499e16379520b34d7718c07e1a4
Noah Kantrowitz <noah@coderanger.net>
2012-02-17 11:00:00 -0800

    Add support for RPM builds. Untested as I do not have a RedHat system to test on.

--- lib/omnibus/project.rb
+++ lib/omnibus/project.rb
@@ -5,6 +5,8 @@ module Omnibus
   class Project
     include Rake::DSL
 
+    PACKAGE_TYPES = ["deb", "rpm"]
+
     attr_reader :name
     attr_reader :description
     attr_reader :dependencies
@@ -30,24 +32,32 @@ module Omnibus
 
     def render_tasks
       namespace :projects do
-        desc "package #{@name}"
-        task @name => (@dependencies.map {|dep| "software:#{dep}"}) do
-          command = ["fpm",
-                     "-s dir",
-                     "-t deb",
-                     "-v 0.0.1",
-                     "-n #{@name}",
-                     "/opt/opscode",
-                     "-m 'Opscode, Inc.'",
-                     "--description 'The full stack of #{@name}'",
-                     "--url http://www.opscode.com"].join(" ")
-          shell = Mixlib::ShellOut.new(command,
-                                       :live_stream => STDOUT,
-                                       :timeout => 3600,
-                                       :cwd => './pkg')
-          shell.run_command
-          shell.error!
+        PACKAGE_TYPES.each do |pkg_type|
+          namespace @name do
+            desc "package #{@name} into a #{pkg_type}"
+            task pkg_type => (@dependencies.map {|dep| "software:#{dep}"}) do
+              Dir.mkdir("pkg") unless File.exists?("pkg")
+              command = ["fpm",
+                   "-s dir",
+                   "-t #{pkg_type}",
+                   "-v 0.0.1",
+                   "-n #{@name}",
+                   "/opt/opscode",
+                   "-m 'Opscode, Inc.'",
+                   "--description 'The full stack of #{@name}'",
+                   "--url http://www.opscode.com"].join(" ")
+              shell = Mixlib::ShellOut.new(command,
+                                           :live_stream => STDOUT,
+                                           :timeout => 3600,
+                                           :cwd => './pkg')
+              shell.run_command
+              shell.error!
+            end
+          end
         end
+
+        desc "package #{@name}"
+        task @name => (PACKAGE_TYPES.map {|pkg_type| "projects:#{@name}:#{pkg_type}"})
       end
     end
   end
