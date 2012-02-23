d6f9754dd9246ab01cea58b701dc25671c93300a
Daniel DeLeo <dan@opscode.com>
2012-02-22 16:19:48 -0800

    sort projects when pretty printing the list of versions

--- lib/omnibus.rb
+++ lib/omnibus.rb
@@ -47,7 +47,8 @@ module Omnibus
       out = ""
       version_map = Omnibus.library.version_map
       width = version_map.keys.max {|a,b| a.size <=> b.size }.size + 3
-      version_map.each do |name, version|
+      version_map.keys.sort.each do |name|
+        version = version_map[name]
         out << "#{name}:".ljust(width) << version.to_s << "\n"
       end
       out
