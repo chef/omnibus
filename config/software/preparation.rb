7a012d538dfaacecaf741af1979110376d886e98
Stephen Delano <stephen@opscode.com>
2012-02-29 16:01:54 -0800

    remove software configs

--- config/software/preparation.rb
+++
@@ -1,8 +0,0 @@
-name "preparation"
-description "the steps required to preprare the build"
-
-build do
-  command "mkdir -p #{install_dir}/embedded/lib"
-  command "mkdir -p #{install_dir}/embedded/bin"
-  command "mkdir -p #{install_dir}/bin"
-end
