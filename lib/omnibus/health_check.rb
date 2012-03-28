cc3e000862a59986d92a98b187169096c5c7e25f
Stephen Delano <stephen@opscode.com>
2012-03-28 13:24:23 -0700

    comment out solaris libs in health check for re-evaluation

--- lib/omnibus/health_check.rb
+++ lib/omnibus/health_check.rb
@@ -15,17 +15,18 @@ module Omnibus
                       /libnsl\.so/,
                       /libfreebl\d\.so/,
                       /libresolv\.so/,
-                      /libaio\.so/,     # solaris
-                      /libavl\.so/,     # solaris
-                      /libdoor\.so/,    # solaris
-                      /libgen\.so/,     # solaris
-                      /libmd\.so/,      # solaris
-                      /libmp\.so/,      # solaris
-                      /libscf\.so/,     # solaris
-                      /libsec\.so/,     # solaris
-                      /libsocket\.so/,  # solaris
-                      /libuutil\.so/,   # solaris
-                      /libcrypt_d\.so/] # solaris
+                      # /libaio\.so/,     # solaris
+                      # /libavl\.so/,     # solaris
+                      # /libdoor\.so/,    # solaris
+                      # /libgen\.so/,     # solaris
+                      # /libmd\.so/,      # solaris
+                      # /libmp\.so/,      # solaris
+                      # /libscf\.so/,     # solaris
+                      # /libsec\.so/,     # solaris
+                      # /libsocket\.so/,  # solaris
+                      # /libuutil\.so/,   # solaris
+                      # /libcrypt_d\.so/] # solaris
+                     ]
 
     def self.run(install_dir)
       ldd_cmd = "find #{install_dir} -type f | xargs ldd"
