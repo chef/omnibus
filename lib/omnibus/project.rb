c29ecbc01b49f58e017a01a4db5d6e3dca024716
Brandon Martin <b@heyomayeah.com>
2012-04-21 13:00:45 -0700

    support freebsd package building with pkg_create add libthr (freebsd) to whitelist

--- lib/omnibus/project.rb
+++ lib/omnibus/project.rb
@@ -121,12 +121,50 @@ module Omnibus
         [ "rpm" ]
       when 'solaris2'
         [ "solaris" ]
+      when 'freebsd'
+        [ "freebsd" ]
       else
         []
       end
     end
 
     private
+
+    def freebsd_pkg_create_command(pkg_type, exclude_file)
+      command_and_opts = ["/usr/ports/Tools/scripts/plist -Md -m /dev/null",
+                          install_path,
+                          "| awk",
+                          "'{if (/@dirrm/)\
+                             {print \"@exec mkdir -p %D/\"$2\"\\n@dirrm \"\$2} else\
+                             print $0}'",
+                          "| pkg_create",
+                          "-c '-Opscode, Inc. - #{@name} - http://www.opscode.com'",
+                          "-d '-The full stack of #{@name}'",
+                          "-p #{install_path}",
+                          "-f -",
+                          "-j",
+                          "#{package_name}-#{build_version}-#{iteration}"]
+      if File.exist?("#{package_scripts_path}/postinst")
+        command_and_opts << "-I '#{package_scripts_path}/postinst'"
+      end
+      if File.exist?("#{package_scripts_path}/postrm")
+        command_and_opts << "-K '#{package_scripts_path}/postrm'"
+      end
+
+      unless @exclusions.nil? || @exclusions.empty?
+        File.open(exclude_file, 'w') do |fd|
+          @exclusions.each do |pattern|
+            fd.puts(pattern)
+          end
+          fd.close
+        end
+        command_and_opts << "-X '#{exclude_file}'"
+      end
+      # There is no good analogue to replaces/obsoletes with pkg_create
+      # so we'll frag it for now..
+      # command_and_opts << " --replaces #{@replaces}" if @replaces
+      command_and_opts
+    end
 
     def fpm_command(pkg_type)
       command_and_opts = ["fpm",
@@ -167,16 +205,24 @@ module Omnibus
           namespace @name do
             desc "package #{@name} into a #{pkg_type}"
             task pkg_type => (@dependencies.map {|dep| "software:#{dep}"}) do
+              exclude_file = 'exclusions.out'
+              if pkg_type == 'freebsd'
+                full_cmd = freebsd_pkg_create_command(pkg_type, exclude_file).join(" ")
+              else
+                full_cmd = fpm_command(pkg_type).join(" ")
+              end
 
-              fpm_full_cmd = fpm_command(pkg_type).join(" ")
-              puts "[project:#{name}] Executing `#{fpm_full_cmd}`"
+              puts "[project:#{name}] Executing `#{full_cmd}`"
 
-              shell = Mixlib::ShellOut.new(fpm_full_cmd,
+              shell = Mixlib::ShellOut.new(full_cmd,
                                            :live_stream => STDOUT,
                                            :timeout => 3600,
                                            :cwd => config.package_dir)
               shell.run_command
               shell.error!
+              if File.exist?(exclude_file)
+                File.delete(exclude_file)
+              end
             end
 
             task pkg_type => config.package_dir
