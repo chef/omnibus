a87f7df3308f7d6fa5d47e75205897b91ff74934
Adam Jacob <adam@opscode.com>
2012-05-04 15:33:28 -0700

    Move to ruby 1.9.3-p320

--- cookbooks/ruby_1.9/recipes/default.rb
+++ cookbooks/ruby_1.9/recipes/default.rb
@@ -30,12 +30,12 @@ end
 # * the source file already exists
 # * ruby 1.9.2 has been installed at the specified patch-level
 #
-remote_file "/tmp/ruby-1.9.2-p180.tar.gz" do
-  source "http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz"
+remote_file "/tmp/ruby-1.9.3-p320.tar.gz" do
+  source "http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p320.tar.gz"
   not_if do
-    ::File.exists?("/tmp/ruby-1.9.2-p180.tar.gz") ||
+    ::File.exists?("/tmp/ruby-1.9.3-p320.tar.gz") ||
       (::File.exists?("/opt/ruby1.9/bin/ruby") &&
-       system("/opt/ruby1.9/bin/ruby --version | grep -q '1.9.2p180'"))
+       system("/opt/ruby1.9/bin/ruby --version | grep -q '1.9.3p320'"))
   end
 end
 
@@ -45,8 +45,8 @@ end
 bash "install ruby-1.9.2" do
   cwd "/tmp"
   code <<-EOH
-tar zxf ruby-1.9.2-p180.tar.gz
-cd ruby-1.9.2-p180
+tar zxf ruby-1.9.3-p320.tar.gz
+cd ruby-1.9.3-p320
 ./configure --prefix=/opt/ruby1.9
 make
 make install
