44b8a951b115937dfcdb079d6b8741bb510e2ab0
Adam Jacob <adam@opscode.com>
2012-05-30 10:47:32 -0700

    Adding osx health check, tarball pkgs

--- lib/omnibus/health_check.rb
+++ lib/omnibus/health_check.rb
@@ -33,7 +33,7 @@ module Omnibus
                       /libstdc\+\+\.so/,
                       /libutil\.so/,
                       /linux-vdso.+/,
-                      /linux-gate\.so/
+                      /linux-gate\.so/,
                       ]
 
     SOLARIS_WHITELIST_LIBS = [
@@ -53,6 +53,21 @@ module Omnibus
                               /libssl.so/,
                               /libuutil\.so/
                              ]
+
+    MAC_WHITELIST_LIBS = [
+      /libobjc\.A\.dylib/,
+      /libSystem\.B\.dylib/,
+      /CoreFoundation/,
+      /Tcl$/,
+      /Cocoa$/,
+      /Carbon$/,
+      /IOKit$/,
+      /Tk$/,
+      /libutil\.dylib/,
+      /libffi\.dylib/,
+      /libncurses\.5\.4\.dylib/,
+      /libiconv/
+    ]
     
     WHITELIST_FILES = [
                        /jre\/bin\/javaws/,
@@ -61,13 +76,92 @@ module Omnibus
                        /jre\/plugin/,
                       ]
 
-    WHITELIST_LIBS.push(*SOLARIS_WHITELIST_LIBS)
+    #WHITELIST_LIBS.push(*SOLARIS_WHITELIST_LIBS)
 
     def self.log(msg)
       puts "[health_check] #{msg}"
     end
 
     def self.run(install_dir)
+      if OHAI.platform == "mac_os_x"
+        bad_libs = health_check_otool(install_dir)
+      else
+        bad_libs = health_check_ldd(install_dir)
+      end
+
+      if bad_libs.keys.length > 0
+        bad_libs.each do |name, lib_hash|
+          lib_hash.each do |lib, linked_libs|
+            linked_libs.each do |linked, count|
+              log "#{name}: #{lib} #{linked} #{count}"
+            end
+          end
+        end
+        raise "Health Check Failed"
+      end
+    end
+
+    def self.health_check_otool(install_dir)
+      otool_cmd = "find #{install_dir}/ -type f | egrep '\.(dylib|bundle)$' | xargs otool -L > otool.out 2>/dev/null"
+      log "Executing `#{otool_cmd}`"
+      shell = Mixlib::ShellOut.new(otool_cmd, :timeout => 3600)
+      shell.run_command
+
+      otool_output = File.read('otool.out')
+
+      current_library = nil
+      bad_libs = {}
+
+      otool_output.each_line do |line|
+        case line
+        when /^(.+):$/
+          current_library = $1
+        when /^\s+(.+) \(.+\)$/
+          linked = $1
+          name = File.basename(linked)
+          bad_libs = check_for_bad_library(install_dir, bad_libs, current_library, name, linked)
+        end
+      end
+
+      File.delete('otool.out')
+
+      bad_libs
+    end
+
+    def self.check_for_bad_library(install_dir, bad_libs, current_library, name, linked)
+      safe = nil
+
+      whitelist_libs = case OHAI.platform
+                       when 'mac_os_x'
+                         MAC_WHITELIST_LIBS
+                       when 'solaris2'
+                         SOLARIS_WHITELIST_LIBS
+                       else
+                         WHITELIST_LIBS
+                       end
+      whitelist_libs.each do |reg| 
+        safe ||= true if reg.match(name)
+      end
+      WHITELIST_FILES.each do |reg|
+        safe ||= true if reg.match(current_library)
+      end
+
+      if !safe && linked !~ Regexp.new(install_dir)
+        bad_libs[current_library] ||= {}
+        bad_libs[current_library][name] ||= {} 
+        if bad_libs[current_library][name].has_key?(linked)
+          bad_libs[current_library][name][linked] += 1 
+        else
+          bad_libs[current_library][name][linked] = 1 
+        end
+      else
+        log "Passed: #{current_library} #{name} #{linked}" if ARGV[0] == 'verbose'
+      end
+
+      bad_libs
+    end
+    
+    def self.health_check_ldd(install_dir)
       #
       # ShellOut has GC turned off during execution, so when we're
       # executing extremely long commands with lots of output, we
@@ -91,25 +185,7 @@ module Omnibus
         when /^\s+(.+) \=\>\s+(.+)( \(.+\))?$/
           name = $1
           linked = $2
-          safe = nil
-          WHITELIST_LIBS.each do |reg| 
-            safe ||= true if reg.match(name)
-          end
-          WHITELIST_FILES.each do |reg|
-            safe ||= true if reg.match(current_library)
-          end
-
-          if !safe && linked !~ Regexp.new(install_dir)
-            bad_libs[current_library] ||= {}
-            bad_libs[current_library][name] ||= {} 
-            if bad_libs[current_library][name].has_key?(linked)
-              bad_libs[current_library][name][linked] += 1 
-            else
-              bad_libs[current_library][name][linked] = 1 
-            end
-          else
-            log "Passed: #{current_library} #{name} #{linked}" if ARGV[0] == 'verbose'
-          end
+          bad_libs = check_for_bad_library(install_dir, bad_libs, current_library, name, linked)
         when /^\s+(.+) \(.+\)$/
           next
         when /^\s+statically linked$/
@@ -127,17 +203,7 @@ module Omnibus
       end
 
       File.delete('ldd.out')
-
-      if bad_libs.keys.length > 0
-        bad_libs.each do |name, lib_hash|
-          lib_hash.each do |lib, linked_libs|
-            linked_libs.each do |linked, count|
-              log "#{name}: #{lib} #{linked} #{count}"
-            end
-          end
-        end
-        raise "Health Check Failed"
-      end
+      bad_libs
     end
 
   end
