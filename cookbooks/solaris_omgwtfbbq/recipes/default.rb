0a63c769c93cd7c6e4639cedfb9bff1660745f45
Stephen Delano <stephen@opscode.com>
2012-08-06 12:37:29 -0700

    rename jenkins user on solaris

--- cookbooks/solaris_omgwtfbbq/recipes/default.rb
+++ cookbooks/solaris_omgwtfbbq/recipes/default.rb
@@ -5,8 +5,7 @@ return unless node['platform'] == "solaris2"
 
 ENV['PATH'] = "/opt/csw/bin:/usr/local/bin:/usr/sfw/bin:/usr/ccs/bin:/usr/sbin:/usr/bin"
 
-template "/etc/default/su" do
-end
+node.default['omnibus']['build-user'] = 'jenkins'
 
 template "/etc/default/login" do
   source "default.login"
