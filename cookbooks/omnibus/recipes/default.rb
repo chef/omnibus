25fae1fda8266cdfa2d698bdd4c5a94711164601
Seth Chisamore <schisamo@opscode.com>
2012-10-16 14:45:22 -0700

    leverage platform_family in omnibus::default

--- cookbooks/omnibus/recipes/default.rb
+++ cookbooks/omnibus/recipes/default.rb
@@ -22,15 +22,15 @@
 # ln -s /var/lib/gems/1.8/bin/* /usr/local/bin
 
 # make certain our chef-solo cache dir exists
-directory "#{Chef::Config[:file_cache_path]}" do
+directory Chef::Config[:file_cache_path] do
   recursive true
   action :create
 end
 
-case node['platform']
-when "ubuntu", "debian"
+case node['platform_family']
+when "debian"
   include_recipe "apt"
-when "centos"
+when "rhel"
   include_recipe "yum::epel"
 when "solaris2"
   include_recipe "opencsw"
@@ -39,7 +39,6 @@ end
 
 include_recipe "build-essential"
 include_recipe "git"
-include_recipe "python"
 
 # install ruby and symlink the binaries to /usr/local
 include_recipe "ruby_1.9"
@@ -50,16 +49,10 @@ include_recipe "ruby_1.9"
 end
 
 # install the packaging related packages
-package_pkgs = value_for_platform(
-  ["ubuntu", "debian"] => {
-    "default" => ["dpkg-dev"]
-  },
-  ["centos"] => {
-    "default" => ["rpm-build"]
-  },
-  ["mac_os_x", "solaris2"] => {
-    "default" => [],
-  }
+package_pkgs = value_for_platform_family(
+  "debian" => ["dpkg-dev"],
+  "rhel" => ["rpm-build"],
+  "default" => []
 )
 package_pkgs.each do |pkg|
   package pkg do
@@ -68,16 +61,10 @@ package_pkgs.each do |pkg|
 end
 
 # install the libxml / libxslt packages
-xml_pkgs = value_for_platform(
-  ["ubuntu", "debian"] => {
-    "default" => ["libxml2", "libxml2-dev", "libxslt1.1", "libxslt1-dev"]
-  },
-  ["centos"] => {
-    "default" => ["libxml2", "libxml2-devel", "libxslt", "libxslt-devel"]
-  },
-  ["mac_os_x", "solaris2"] => {
-    "default" => [],
-  }
+xml_pkgs = value_for_platform_family(
+  "debian" => ["libxml2", "libxml2-dev", "libxslt1.1", "libxslt1-dev"],
+  "rhel" => ["libxml2", "libxml2-devel", "libxslt", "libxslt-devel"],
+  "default" => []
 )
 xml_pkgs.each do |pkg|
   package pkg do
@@ -85,31 +72,53 @@ xml_pkgs.each do |pkg|
   end
 end
 
-if node['platform'] == "solaris2"
-  %w{libxml2_dev libxslt_dev libssl_dev libyaml}.each do |pkg|
-    opencsw pkg
-  end
-end
+case node['platform_family']
+when "solaris2"
 
-if node['platform'] != "solaris2"
-  %w{libtool help2man gettext texinfo}.each do |name|
-    package name
-  end
-else
-  %w{libtool help2man ggettext texinfo}.each do |pkg|
+  %w{libxml2_dev libxslt_dev libssl_dev libyaml libtool help2man ggettext texinfo}.each do |pkg|
     opencsw pkg
   end
+
   link "/opt/csw/bin/gettext" do
     to "/opt/csw/bin/gettext"
   end
+
+else
+
+  %w{libtool help2man gettext texinfo}.each do |name|
+    package name
+  end
+
+  # Turn off strict host key checking for github
+  execute "disable-host-key-checking-github" do
+    command "echo '\nHost github.com\n\tStrictHostKeyChecking no' >> /etc/ssh/ssh_config"
+    not_if "cat /etc/ssh/ssh_config | grep github.com"
+    only_if { ::File.exists?("/etc/ssh/ssh_config") }
+  end
+  # Ensure SSH_AUTH_SOCK is honored under sudo
+  execute "make-sudo-honor-ssh_auth_sock" do
+    command "echo '\nDefaults env_keep+=SSH_AUTH_SOCK' >> /etc/sudoers"
+    not_if "cat /etc/sudoers | grep SSH_AUTH_SOCK"
+    only_if { ::File.exists?("/etc/sudoers") }
+  end
+
 end
 
-#bash "install python packages" do
-#  code <<BASH
-#pip install Sphinx==1.1.2
-#pip install Pygments==1.4
-#BASH
-#end
+case node['platform_family']
+when "debian", "rhel"
+
+  include_recipe "python"
+
+  python_pip "Sphinx" do
+    version "1.1.3"
+    action :install
+  end
+
+  python_pip "Pygments" do
+    version "1.4"
+    action :install
+  end
+end
 
 node['omnibus']['install-dirs'].each do |d|
   directory d do
@@ -123,19 +132,4 @@ directory "/var/cache/omnibus" do
   mode "755"
   owner node["omnibus"]["build-user"]
   recursive true
-end
-
-unless node['platform'] == "solaris2"
-  # Turn off strict host key checking for github
-  execute "disable-host-key-checking-github" do
-    command "echo '\nHost github.com\n\tStrictHostKeyChecking no' >> /etc/ssh/ssh_config"
-    not_if "cat /etc/ssh/ssh_config | grep github.com"
-    only_if { ::File.exists?("/etc/ssh/ssh_config") }
-  end
-  # Ensure SSH_AUTH_SOCK is honored under sudo
-  execute "make-sudo-honor-ssh_auth_sock" do
-    command "echo '\nDefaults env_keep+=SSH_AUTH_SOCK' >> /etc/sudoers"
-    not_if "cat /etc/sudoers | grep SSH_AUTH_SOCK"
-    only_if { ::File.exists?("/etc/sudoers") }
-  end
 end
