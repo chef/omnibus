742c6d0b4c3f11b15e057db420d38f70c74e475d
sdelano <stephen@opscode.com>
2012-11-20 10:45:43 -0800

    ruby cookbook: windows compat

--- cookbooks/omnibus/recipes/default.rb
+++ cookbooks/omnibus/recipes/default.rb
@@ -37,6 +37,7 @@ when "solaris2"
   include_recipe "solaris_omgwtfbbq"
 when "windows"
   include_recipe "wix"
+  # include_recipe "7-zip"
 end
 
 include_recipe "build-essential"
@@ -44,9 +45,12 @@ include_recipe "git"
 
 # install ruby and symlink the binaries to /usr/local
 include_recipe "ruby_1.9"
-%w{ruby gem rake bundle fpm ronn}.each do |bin|
-  link "/usr/local/bin/#{bin}" do
-    to "/opt/ruby1.9/bin/#{bin}"
+
+unless node['platform_family'] == 'windows'
+  %w{ruby gem rake bundle fpm ronn}.each do |bin|
+    link "/usr/local/bin/#{bin}" do
+      to "/opt/ruby1.9/bin/#{bin}"
+    end
   end
 end
 
@@ -84,6 +88,12 @@ when "solaris2"
   link "/opt/csw/bin/gettext" do
     to "/opt/csw/bin/gettext"
   end
+
+when "windows"
+
+  #
+  # nothing to do here
+  #
 
 else
 
