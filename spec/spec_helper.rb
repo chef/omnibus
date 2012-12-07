c279379b39352cc5a96c75953da3a0b5030b76cd
Christopher Maier <cm@opscode.com>
2012-12-07 13:42:57 -0500

    Add some RSpec!
    Test software version overrides

---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+# Why isn't this already required in omnibus?
+require 'rake/dsl_definition'
+
+require 'omnibus'
+
+module Omnibus
+  module RSpec
+
+    SPEC_DATA = File.expand_path(File.join(File.dirname(__FILE__), "data"))
+
+    def software_path(name)
+      File.join(SPEC_DATA, "software", "#{name}.rb")
+    end
+
+  end
+end
+
+RSpec.configure do |config|
+  config.include Omnibus::RSpec
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+end
+
