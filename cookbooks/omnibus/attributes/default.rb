ca8243d4460e05cec85a875509695e2bc3cff6b3
Seth Chisamore <schisamo@opscode.com>
2012-12-19 11:11:13 -0500

    add default cookbook attribute for ['omnibus']['cache-dirs']
    Fix to stop your friendly neighborhood "undefined method `each' for
    nil:NilClass" exception when the omnibus::default recipe is executed.
    Existing (*nix) Omnibus projects do not have this value set in their
    Vagrantfile's chef.json.

--- cookbooks/omnibus/attributes/default.rb
+++ cookbooks/omnibus/attributes/default.rb
@@ -18,3 +18,4 @@
 default["omnibus"]["build-user"] = "vagrant"
 default["omnibus"]["build-folder"] = "/home/vagrant/omnibus"
 default["omnibus"]["install-dirs"] = []
+default['omnibus']['cache-dirs'] = ["/var/cache/omnibus"]
