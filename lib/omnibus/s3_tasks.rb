56797adb7bf24c267b1ab997750cf02a05369cc1
Christopher Maier <cm@opscode.com>
2013-03-23 14:28:52 -0700

    Simplify s3 tasks
    We don't really need to go through the ceremony of creating more
    classes and methods.

--- lib/omnibus/s3_tasks.rb
+++ lib/omnibus/s3_tasks.rb
@@ -5,9 +5,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,48 +15,36 @@
 # limitations under the License.
 #
 
-module Omnibus
-  module S3Tasks
-    extend Rake::DSL
+require 'rake'
+require 'omnibus/s3_cacher'
 
-    def self.define!
-      require 'uber-s3'
+# TODO: make the setup of S3-related tasks dependent on the value of
+# Omnibus.config.use_s3_caching.  This will require a change to the
+# order of how things are loaded.
 
-      namespace :s3 do
-        desc "List source packages which have the correct source package in the S3 cache"
-        task :existing do
-          S3Cache.new.list.each {|s| puts s.name}
-        end
+namespace :s3 do
+  desc "List source packages which have the correct source package in the S3 cache"
+  task :existing do
+    S3Cache.new.list.each {|s| puts s.name}
+  end
 
-        desc "List all cached files (by S3 key)"
-        task :list do
-          S3Cache.new.list_by_key.each {|k| puts k}
-        end
+  desc "List all cached files (by S3 key)"
+  task :list do
+    S3Cache.new.list_by_key.each {|k| puts k}
+  end
 
-        desc "Lists source packages that are required but not yet cached"
-        task :missing do
-          S3Cache.new.missing.each {|s| puts s.name}
-        end
+  desc "Lists source packages that are required but not yet cached"
+  task :missing do
+    S3Cache.new.missing.each {|s| puts s.name}
+  end
 
-        desc "Fetches missing source packages to local tmp dir"
-        task :fetch do
-          S3Cache.new.fetch_missing
-        end
+  desc "Fetches missing source packages to local tmp dir"
+  task :fetch do
+    S3Cache.new.fetch_missing
+  end
 
-        desc "Populate the S3 Cache"
-        task :populate do
-          S3Cache.new.populate
-        end
-
-      end
-    rescue LoadError
-
-      desc "S3 tasks not available"
-      task :s3 do
-        puts(<<-F)
-The `uber-s3` gem is required to cache new source packages in S3.
-F
-      end
-    end
+  desc "Populate the S3 Cache"
+  task :populate do
+    S3Cache.new.populate
   end
 end
