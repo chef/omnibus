daaa8991805f9a17a0f70bab96ddf0f423fbf931
Christopher Maier <cm@opscode.com>
2013-03-24 15:50:25 -0700

    Overrides documentation

--- lib/omnibus/overrides.rb
+++ lib/omnibus/overrides.rb
@@ -5,15 +5,17 @@ module Omnibus
 
     DEFAULT_OVERRIDE_FILE_NAME = "omnibus.overrides"
 
-    # Parses a file of override information into a hash.
+    # Parses a file of override information into a Hash.
     #
     # Each line of the file must be of the form
     #
-    # <package_name> <version>
+    #
+    #     <package_name> <version>
     #
     # where the two pieces of data are separated by whitespace.
     #
-    # Returns +nil+ if +file+ is +nil+.
+    # @param file [String] the path to an overrides file
+    # @return [Hash, nil]
     def self.parse_file(file)
       if file
         File.readlines(file).inject({}) do |acc, line|
@@ -22,13 +24,13 @@ module Omnibus
           unless info.count == 2
             raise ArgumentError, "Invalid overrides line: '#{line.chomp}'"
           end
-          
+
           package, version = info
-          
+
           if acc[package]
             raise ArgumentError, "Multiple overrides present for '#{package}' in overrides file #{file}!"
           end
-          
+
           acc[package] = version
           acc
         end
@@ -44,23 +46,24 @@ module Omnibus
       path = File.expand_path(file)
       File.exist?(path) ? path : nil
     end
-    
+
     # Return a hash of override information.  If no such information
     # can be found, the hash will be empty
+    #
+    # @return [Hash]
     def self.overrides
       file = resolve_override_file
       overrides = parse_file(file)
-            
+
       if overrides
         puts "********************************************************************************"
         puts "Using Overrides from #{Omnibus::Overrides.resolve_override_file}"
         pp overrides
         puts "********************************************************************************"
       end
-      
+
       overrides || {}
-    end    
+    end
 
   end
 end
-
