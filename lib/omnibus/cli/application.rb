7a7ede4839dda4cc0e670e826ae048d9da5f3b17
Seth Chisamore <schisamo@opscode.com>
2013-04-03 14:59:17 -0400

    CLI: slight refactor and subcommand support
    The first subcommand up will be `omnibus build` which will contain
    support for building projects AND eventually software components.

---
+++ lib/omnibus/cli/application.rb
@@ -0,0 +1,89 @@
+#
+# Copyright:: Copyright (c) 2012 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'omnibus'
+require 'omnibus/cli/base'
+require 'omnibus/cli/build'
+
+module Omnibus
+  module CLI
+    class Application < Base
+
+      method_option :purge,
+        :type => :boolean,
+        :default => false,
+        :desc => "Remove ALL files generated during the build (including packages)."
+      desc "clean PROJECT", "Remove temporary files generated during the build process."
+      def clean(project_name)
+        load_omnibus_projects!(options[:path], options[:config])
+        project = load_project!(project_name)
+
+        deletion_list = []
+        deletion_list << Dir.glob("#{Omnibus.config.source_dir}/**/*")
+        deletion_list << Dir.glob("#{Omnibus.config.build_dir}/**/*")
+
+        if options[:purge]
+          deletion_list << Dir.glob("#{Omnibus.config.package_dir}/**/*")
+          deletion_list << Dir.glob("#{Omnibus.config.cache_dir}/**/*")
+          deletion_list << project.install_path if File.exist?(project.install_path)
+        end
+
+        deletion_list.flatten!
+        deletion_list.each{|f| remove_file(f) }
+      end
+
+      desc "project PROJECT", "Creates a skeletal Omnibus project"
+      def project(name)
+        target = File.join(options[:path], name)
+        install_path = File.join("/opt", name)
+        opts = {
+          :name => name,
+          :install_path => install_path
+        }
+
+        create_file(File.join(target, "config", "software", "README.md"),
+                    "Software definitions for your project's dependencies go here!")
+        template(File.join("Gemfile.erb"), File.join(target, "Gemfile"), opts)
+        template(File.join("gitignore.erb"), File.join(target, ".gitignore"), opts)
+        template(File.join("project.rb.erb"), File.join(target, "config", "projects", "#{name}.rb"), opts)
+        template(File.join("README.md.erb"), File.join(target, "README.md"), opts)
+
+        # render out stub packge scripts
+        %w{ makeselfinst postinst postrm }.each do |package_script|
+          script_path = File.join(target, "package-scripts", name, package_script)
+          template_path = File.join("package_scripts", "#{package_script}.erb")
+          # render the package script
+          template(template_path, script_path, opts)
+          # ensure the package script is executable
+          FileUtils.chmod(0755, script_path)
+        end
+      end
+
+      desc "version", "Display version information"
+      def version
+        say("Omnibus: #{Omnibus::VERSION}", :yellow)
+      end
+
+      ###########################################################################
+      # Subcommands
+      ###########################################################################
+      desc "build [COMMAND]", "Perform build-related tasks."
+      subcommand "build", Omnibus::CLI::Build
+
+    end
+  end
+end
