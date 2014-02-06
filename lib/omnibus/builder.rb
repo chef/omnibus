fa9259c7a92c256d50492ae8822e7d8355f4a2bb
danielsdeleo <dan@opscode.com>
2014-02-05 16:07:00 -0800

    Remove todo for null object pattern.
    See: http://en.wikipedia.org/wiki/Null_Object_pattern

--- lib/omnibus/builder.rb
+++ lib/omnibus/builder.rb
@@ -345,9 +345,6 @@ module Omnibus
 
   end
 
-  # @todo What's the point of this class?  Can we not just detect that
-  #   there are no commands in {Omnibus::Builder#build} and output the
-  #   appropriate message?  Seems like a lot of extra ceremony.
   class NullBuilder < Builder
 
     def build
