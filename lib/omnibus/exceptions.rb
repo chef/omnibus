d3d67078fe029a97a4c1197f20336b34a2891568
Seth Vargo <sethvargo@gmail.com>
2014-05-08 18:45:15 -0400

    Fix formatting on S3 error

--- lib/omnibus/exceptions.rb
+++ lib/omnibus/exceptions.rb
@@ -41,29 +41,27 @@ module Omnibus
     end
 
     def to_s
-      """
-      One or more required S3 configuration values is missing.
+      <<-EOH
+One or more required S3 configuration values is missing.
 
-      Your effective configuration was the following:
+Your effective configuration was the following:
 
-          s3_bucket     => #{@s3_bucket.inspect}
-          s3_access_key => #{@s3_access_key.inspect}
-          s3_secret_key => #{@s3_secret_key.inspect}
+    s3_bucket     #{@s3_bucket.inspect}
+    s3_access_key #{@s3_access_key.inspect}
+    s3_secret_key #{@s3_secret_key.inspect}
 
-      If you truly do want S3 caching, you should add values similar
-      to the following in your Omnibus config file:
+If you truly do want S3 caching, you should add values similar to the following
+in your Omnibus config file:
 
-            s3_bucket      ENV['S3_BUCKET_NAME']
-            s3_access_key  ENV['S3_ACCESS_KEY']
-            s3_secret_key  ENV['S3_SECRET_KEY']
+    s3_bucket     ENV['S3_BUCKET_NAME']
+    s3_access_key ENV['S3_ACCESS_KEY']
+    s3_secret_key ENV['S3_SECRET_KEY']
 
-      Note that you are not required to use environment variables as
-      illustrated (and the ones listed have no special significance in
-      Omnibus), but it is encouraged to prevent spread of sensitive
-      information and inadvertent check-in of same to version control
-      systems.
-
-      """
+Note that you are not required to use environment variables as illustrated (and
+the ones listed have no special significance in Omnibus), but it is encouraged
+to prevent spread of sensitive information and inadvertent check-in of same to
+version control systems.
+      EOH
     end
   end
 
