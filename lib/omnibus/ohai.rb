da5e4d77a4d09e2b3de0056fa435c2efb310293a
Seth Vargo <sethvargo@gmail.com>
2014-05-08 22:37:40 -0400

    Just fucking delegate everything to Ohai

--- lib/omnibus/ohai.rb
+++ lib/omnibus/ohai.rb
@@ -20,15 +20,7 @@ module Omnibus
   class Ohai
     class << self
       def method_missing(m, *args, &block)
-        if respond_to_missing?(m)
-          ohai.send(m, *args, &block)
-        else
-          super
-        end
-      end
-
-      def respond_to_missing?(m, include_private = false)
-        ohai.respond_to_missing?(m, include_private) || ohai.attribute?(m)
+        ohai.send(m, *args, &block)
       end
 
       private
