17df7b420916d27395814ad89a6b18312adb123c
Seth Vargo <sethvargo@gmail.com>
2014-07-07 16:20:06 -0400

    Remove debugging output that snuck in

--- lib/omnibus/cli.rb
+++ lib/omnibus/cli.rb
@@ -68,10 +68,6 @@ module Omnibus
       project = Omnibus.project(name)
       raise ProjectNotFound.new(name) unless project
 
-      say("Platform: #{Ohai['platform']}")
-      say("Platform Family: #{Ohai['platform_family']}")
-      say("Platform Version: #{Ohai['platform_version']}")
-
       say("Building #{project.name} #{project.build_version}...")
       project.build_me
     end
