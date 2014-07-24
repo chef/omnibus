5320d10b76272a5c201a691d83430ead5533b287
Seth Vargo <sethvargo@gmail.com>
2014-07-24 12:12:53 -0400

    Remove specs for Config.override_file

--- spec/unit/config_spec.rb
+++ spec/unit/config_spec.rb
@@ -39,7 +39,6 @@ module Omnibus
     include_examples 'a configurable', :build_dmg, true
     include_examples 'a configurable', :dmg_window_bounds, '100, 100, 750, 600'
     include_examples 'a configurable', :dmg_pkg_position, '535, 50'
-    include_examples 'a configurable', :override_file, nil
     include_examples 'a configurable', :local_software_dirs, []
     include_examples 'a configurable', :software_gems, ['omnibus-software']
     include_examples 'a configurable', :solaris_compiler, nil
