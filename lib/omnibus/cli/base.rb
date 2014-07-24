a462fe65b0583ba061f8592ae909bd5cc41550ab
Seth Vargo <sethvargo@gmail.com>
2014-07-24 12:00:59 -0400

    Remove deprecated CLI and tests

--- lib/omnibus/cli/base.rb
+++ lib/omnibus/cli/base.rb
@@ -111,7 +111,3 @@ module Omnibus
     end
   end
 end
-
-# Include the deprecations from previous versions of Omnibus. This should be
-# removed in the next major version of Omnibus.
-require_relative 'deprecated'
