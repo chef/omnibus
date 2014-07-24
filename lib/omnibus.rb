263343af7fee8dcb2dab9b3ccba664b52896c3cd
Seth Vargo <sethvargo@gmail.com>
2014-07-24 16:34:15 -0400

    Remove Omnibus.process_dsl_files method

--- lib/omnibus.rb
+++ lib/omnibus.rb
@@ -146,20 +146,6 @@ module Omnibus
     end
 
     #
-    # Expand all {Project} and {Software} objects for all DSL files in the
-    # configured paths.
-    #
-    # @return [void]
-    #
-    def process_dsl_files
-      Omnibus.projects.each do |project|
-        project.dependencies.each do |dependency|
-          recursively_load_dependency(dependency, project)
-        end
-      end
-    end
-
-    #
     # All {Project} instances that have been loaded.
     #
     # @return [Array<:Project>]
