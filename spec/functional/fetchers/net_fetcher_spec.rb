f32bd9a577c6863718a90fefbc13c1ea3fb46878
Seth Vargo <sethvargo@gmail.com>
2014-07-28 17:50:42 -0400

    Fix failing tests by switching to a real software

--- spec/functional/fetchers/net_fetcher_spec.rb
+++ spec/functional/fetchers/net_fetcher_spec.rb
@@ -4,14 +4,14 @@ module Omnibus
   describe NetFetcher do
     include_examples 'a software'
 
-    let(:source_url) { fixture_path('source/zlib-1.2.8.tar.gz') }
-    let(:source_md5) { '44d667c142d7cda120332623eab69f40' }
+    let(:source_url) { 'http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz' }
+    let(:source_md5) { '00b516f4704d4a7cb50a1d97e6e8e15b' }
     let(:source) do
       { url: source_url, md5: source_md5 }
     end
 
     let(:downloaded_file) { subject.send(:downloaded_file) }
-    let(:extracted) { File.join(source_dir, 'zlib-1.2.8') }
+    let(:extracted) { File.join(source_dir, 'bzip2-1.0.6') }
 
     subject { described_class.new(software) }
 
