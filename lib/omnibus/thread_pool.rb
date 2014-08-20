e5a5c309758526f1e6dab65b4c9d23130e1176ac
Mike Heijmans <parabuzzle@gmail.com>
2014-08-20 13:50:14 -0700

    make the thread pool a bit smarter and clean up the code

--- lib/omnibus/thread_pool.rb
+++ lib/omnibus/thread_pool.rb
@@ -3,8 +3,13 @@ require 'thread'
 module Omnibus
 
   class ThreadPool
+
+    attr_reader :queue, :pool, :jobs
+
     def initialize(poolsize)
       @queue = Queue.new
+      @mutex = Mutex.new
+      @jobs = 0
       @poolsize = poolsize  
       @pool = Array.new(@poolsize) do |i|
         Thread.new do
@@ -13,6 +18,7 @@ module Omnibus
             loop do
               job, args = @queue.pop
               job.call(*args)
+              @mutex.synchronize { @jobs -= 1 }
             end
           end
         end
@@ -20,7 +26,18 @@ module Omnibus
     end
   
     def launch(*args, &block)
+      @mutex.synchronize { @jobs += 1 }
       @queue << [block, args]
+    end
+
+    def state
+      @mutex.synchronize {
+        if @jobs > 0
+          return 'working'
+        else
+          return 'idle'
+        end
+      }
     end
   
     def stop
