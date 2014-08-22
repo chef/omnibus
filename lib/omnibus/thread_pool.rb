3e5581b622f1cb7b9ce22cf6d753e95b69373d04
Seth Vargo <sethvargo@gmail.com>
2014-08-22 17:49:34 -0400

    Update the thread pool to accept a block and choose more semantic names

--- lib/omnibus/thread_pool.rb
+++ lib/omnibus/thread_pool.rb
@@ -1,50 +1,109 @@
+#
+# Copyright 2014 Mike Heijmans
+# Copyright 2014 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 require 'thread'
 
 module Omnibus
-
   class ThreadPool
 
-    attr_reader :queue, :pool, :jobs
+    #
+    # Create a new thread pool of the given size. If a block is given, it is
+    # assumed the thread pool is wrapping an operation and will block until all
+    # operations complete.
+    #
+    # @example Using a block
+    #   ThreadPool.new(5) do |pool|
+    #     complex_things.each do |thing|
+    #       pool.schedule { thing.run }
+    #     end
+    #   end
+    #
+    # @example Using the object
+    #   pool = ThreadPool.new(5)
+    #   # ...
+    #   pool.schedule { complex_operation_1 }
+    #   pool.schedule { complex_operation_2 }
+    #   # ...
+    #   pool.schedule { complex_operation_4 }
+    #   # ...
+    #   pool.shutdown
+    #
+    #   # or
+    #
+    #   at_exit { pool.shutdown }
+    #
+    # @param [Integer] size
+    #   the number of items to put in the thread pool
+    #
+    def initialize(size)
+      @size = size
+      @jobs = Queue.new
 
-    def initialize(poolsize)
-      @queue = Queue.new
-      @mutex = Mutex.new
-      @jobs = 0
-      @poolsize = poolsize  
-      @pool = Array.new(@poolsize) do |i|
+      @pool = Array.new(@size) do |i|
         Thread.new do
+          Thread.abort_on_exception = true
           Thread.current[:id] = i
-          catch(:close) do
+
+          catch(:exit) do
             loop do
-              job, args = @queue.pop
+              job, args = @jobs.pop
               job.call(*args)
-              @mutex.synchronize { @jobs -= 1 }
             end
           end
         end
       end
-    end
-  
-    def launch(*args, &block)
-      @mutex.synchronize { @jobs += 1 }
-      @queue << [block, args]
+
+      if block_given?
+        yield self
+        shutdown
+      end
     end
 
-    def state
-      @mutex.synchronize {
-        if @jobs > 0
-          return 'working'
-        else
-          return 'idle'
-        end
-      }
+    #
+    # Schedule a single item onto the queue. If arguments are given, those
+    # arguments are used when calling the block in the queue. This is useful
+    # if you have arguments that you need to pass in from a parent binding.
+    #
+    # @param [Object, Array<Object>] args
+    #   the arguments to pass to the block when calling
+    # @param [Proc] block
+    #   the block to execute
+    #
+    # @return [void]
+    #
+    def schedule(*args, &block)
+      @jobs << [block, args]
     end
-  
-    def stop
-      @poolsize.times do
-        launch { throw :close }
+
+    #
+    # Stop the thread pool. This method quietly injects an exit clause into the
+    # queue (sometimes called "poison") and then waits for all threads to
+    # exit.
+    #
+    # @return [true]
+    #
+    def shutdown
+      @size.times do
+        schedule { throw :exit }
       end
+
       @pool.map(&:join)
+
+      true
     end
   end
 end
