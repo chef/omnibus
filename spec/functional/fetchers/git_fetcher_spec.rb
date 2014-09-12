chef/omnibus

commit 2f240e4882a42afd0724467af32e94add19670e5

Refactor and simplify fetchers
This is a rather large commit, but here was no easy way to split it up
into smaller pieces. This commit refactors fetchers to use the abstract
Fetcher class and adds more documentation and consistency to the
fetcher methods.

lib/omnibus/exceptions.rb
@@ -353,4 +353,20 @@ another attribute or method that requires a `source' (URI) is defined.
 EOH
     end
   end
+
+  class ChecksumMismatch < Error
+    def initialize(software, expected, actual)
+      super <<-EOH
+Verification for #{software.name} failed due to a checksum mismatch:
+
+expected: #{expected}
+actual:   #{actual}
+
+This added security check is used to prevent MITM attacks when downloading the
+remote file. If you have updated the version or URL for the download, you will
+also need to update the checksum value. You can find the checksum value on the
+software publisher's website.
+EOH
+    end
+  end
 end

lib/omnibus/fetchers/git_fetcher.rb
@@ -15,202 +15,167 @@
 #
 
 module Omnibus
-  # Fetcher implementation for projects in git.
   class GitFetcher < Fetcher
-    attr_reader :source
-    attr_reader :project_dir
-    attr_reader :version
-
-    def initialize(software)
-      @name         = software.name
-      @source       = software.source
-      @version      = software.version
-      @project_dir  = software.project_dir
-      super
-    end
-
-    def description
-      <<-EOH.gsub(/^ {8}/, '').strip
-        repo URI:       #{@source[:git]}
-        local location: #{@project_dir}
-      EOH
-    end
-
-    def version_guid
-      "git:#{current_revision}".chomp
-    rescue
-    end
-
-    def clean
-      if existing_git_clone?
-        log.info(log_key) { 'Cleaning existing build' }
-        shellout!('git clean -fdx', cwd: project_dir)
-      end
-    rescue Exception => e
-      ErrorReporter.new(e, self).explain("Failed to clean git repository '#{@source[:git]}'")
-      raise
-    end
-
+    #
+    # A fetch is required if the git repository is not cloned or if the local
+    # revision does not match the desired revision.
+    #
+    # @return [true, false]
+    #
     def fetch_required?
-      !existing_git_clone? || !current_rev_matches_target_rev?
+      !(cloned? && same_revision?)
     end
 
+    #
+    # The version identifier for this git location. This is computed using the
+    # current revision on disk.
+    #
+    # @return [String]
+    #
+    def version_guid
+      "git:#{current_revision}"
+    end
+
+    #
+    # Clean the project directory by removing the contents from disk.
+    #
+    # @return [true, false]
+    #   true if the project directory was removed, false otherwise
+    #
+    def clean
+      if cloned?
+        log.info(log_key) { 'Cleaning existing clone' }
+        git('clean -fdx')
+        true
+      else
+        false
+      end
+    end
+
+    #
+    # Fetch (clone) or update (fetch) the remote git repository.
+    #
+    # @return [void]
+    #
     def fetch
-      retries ||= 0
-      if existing_git_clone?
-        fetch_updates unless current_rev_matches_target_rev?
+      log.info(log_key) { "Fetching from `#{source_url}'" }
+
+      create_required_directories
+
+      if cloned?
+        git_fetch unless same_revision?
       else
-        clone
-        checkout
-      end
-    rescue Exception => e
-      if retries >= 3
-        ErrorReporter.new(e, self).explain("Failed to fetch git repository '#{@source[:git]}'")
-        raise
-      else
-        # Deal with github failing all the time :(
-        time_to_sleep = 5 * (2**retries)
-        retries += 1
-        log.warn(log_key) do
-          "git clone/fetch failed for #{@source} #{retries} time(s). " \
-          "Retrying in #{time_to_sleep}s..."
-        end
-        sleep(time_to_sleep)
-        retry
+        git_clone
+        git_checkout
       end
     end
 
-    # Return the target sha to be used during build caching
-    # This overrides the cases where software.version is similar to
-    # master, 11-stable etc..
+    #
+    # The version for this item in the cache. The is the parsed revision of the
+    # item on disk.
+    #
+    # @return [String]
+    #
     def version_for_cache
-      target_revision
+      "revision:#{current_revision}"
     end
 
     private
 
-    def clone
-      log.info(log_key) { 'Cloning the source from git' }
-      shellout!("git clone #{@source[:git]} #{project_dir}")
+    #
+    # The URL where the git source should be downloaded from.
+    #
+    # @return [String]
+    #
+    def source_url
+      source[:git]
     end
 
-    def checkout
-      sha_ref = target_revision
-      shellout!("git checkout #{sha_ref}", cwd: project_dir)
-    end
-
-    def fetch_updates
-      log.info(log_key) do
-        "Fetching updates and resetting to revision '#{target_revision}'"
-      end
-
-      fetch_cmd = "git fetch origin && " \
-                  "git fetch origin --tags && " \
-                  "git reset --hard #{target_revision}"
-      shellout!(fetch_cmd, cwd: project_dir)
-    end
-
-    def existing_git_clone?
+    #
+    # Determine if the clone exists.
+    #
+    # @return [true, false]
+    #
+    def cloned?
       File.exist?("#{project_dir}/.git")
     end
 
-    def current_rev_matches_target_rev?
-      current_revision && current_revision.strip.to_i(16) == target_revision.strip.to_i(16)
+    #
+    # Clone the +source_url+ into the +project_dir+.
+    #
+    # @return [void]
+    #
+    def git_clone
+      git("clone #{source_url} .")
     end
 
+    #
+    # Checkout the +target_revision+.
+    #
+    # @return [void]
+    #
+    def git_checkout
+      git("fetch --all")
+      git("checkout #{target_revision}")
+    end
+
+    #
+    # Fetch the remote tags and refs, and reset to +target_revision+.
+    #
+    # @return [void]
+    #
+    def git_fetch
+      git("fetch --all")
+      git("reset --hard #{target_revision}")
+    end
+
+    #
+    # The current revision for the cloned checkout.
+    #
+    # @return [String]
+    #
     def current_revision
-      return @current_rev if @current_rev
-
-      cmd = shellout!('git rev-parse HEAD', cwd: project_dir)
-      stdout = cmd.stdout
-
-      @current_rev = sha_hash?(stdout) ? stdout : nil
-      @current_rev
+      @current_revision ||= git('rev-parse HEAD').stdout.strip
+    rescue Mixlib::ShellOut::ShellCommandFailed
+      nil
     end
 
+    #
+    # The target revision from the user.
+    #
+    # @return [String]
+    #
     def target_revision
-      @target_rev ||= if sha_hash?(version)
-                        version
-                      else
-                        revision_from_remote_reference(version)
-                      end
-    end
-
-    def sha_hash?(rev)
-      rev =~ /^[0-9a-f]{40}$/
-    end
-
-    # Return the SHA corresponding to ref. If ref is an annotated tag,
-    # return the SHA that was tagged not the SHA of the tag itself.
-    def revision_from_remote_reference(ref)
-      retries ||= 0
-      # execute `git ls-remote` the trailing '*' does globbing. This
-      # allows us to return the SHA of the tagged commit for annotated
-      # tags. We take care to only return exact matches in
-      # process_remote_list.
-      cmd = shellout!("git ls-remote origin #{ref}*", cwd: project_dir)
-      commit_ref = process_remote_list(cmd.stdout, ref)
-
-      unless commit_ref
-        raise UnresolvableGitReference.new("Could not resolve `#{ref}' to a SHA.")
-      end
-      commit_ref
-    rescue UnresolvableGitReference => e # skip retries
-      ErrorReporter.new(e, self).explain(<<-E)
-Command `#{cmd}' did not find a commit for reference `#{ref}'.
-The tag or branch you're looking for doesn't exist on the remote repo.
-If your project uses version tags like v1.2.3, include the 'v' in your
-software's version.
-E
-      raise
-    rescue Exception => e
-      if retries >= 3
-        ErrorReporter.new(e, self).explain("Failed to find any commits for the ref '#{ref}'")
-        raise
+      @target_revision ||= git("rev-parse #{version}").stdout.strip
+    rescue Mixlib::ShellOut::ShellCommandFailed => e
+      if e.message.include?('ambiguous argument')
+        @target_revision = git("rev-parse origin/#{version}").stdout.strip
+        @target_revision
       else
-        # Deal with github failing all the time :(
-        time_to_sleep = 5 * (2**retries)
-        retries += 1
-        log.warn(log_key) do
-          "git ls-remote failed for #{@source} #{retries} time(s). " \
-          "Retrying in #{time_to_sleep}s..."
-        end
-        sleep(time_to_sleep)
-        retry
+        log.warn { 'Could not determine target revision!' }
+        nil
       end
     end
 
-    def process_remote_list(stdout, ref)
-      # Dereference annotated tags.
-      #
-      # Output will look like this:
-      #
-      # a2ed66c01f42514bcab77fd628149eccb4ecee28        refs/tags/rel-0.11.0
-      # f915286abdbc1907878376cce9222ac0b08b12b8        refs/tags/rel-0.11.0^{}
-      #
-      # The SHA with ^{} is the commit pointed to by an annotated
-      # tag. If ref isn't an annotated tag, there will not be a line
-      # with trailing ^{}.
-      #
-      # We'll return the SHA corresponding to the ^{} which is the
-      # commit pointed to by an annotated tag. If no such commit
-      # exists (not an annotated tag) then we return the SHA of the
-      # ref.  If nothing matches, return "".
-      lines = stdout.split("\n")
-      matches = lines.map { |line| line.split("\t") }
-      # first try for ^{} indicating the commit pointed to by an
-      # annotated tag
-      tagged_commit = matches.find { |m| m[1].end_with?("#{ref}^{}") }
-      if tagged_commit
-        tagged_commit[0]
-      else
-        found = matches.find { |m| m[1].end_with?("#{ref}") }
-        if found
-          found[0]
-        else
-          nil
-        end
-      end
+    #
+    # Determine if the given revision matches the current revision.
+    #
+    # @return [true, false]
+    #
+    def same_revision?
+      current_revision == target_revision
+    end
+
+    #
+    # Execute the given git command, inside the +project_dir+.
+    #
+    # @see Util#shellout!
+    #
+    # @return [Mixlib::ShellOut]
+    #   the shellout object
+    #
+    def git(command)
+      shellout!("git #{command}", cwd: project_dir)
     end
   end
 end

spec/functional/fetchers/git_fetcher_spec.rb
@@ -1,0 +1,132 @@
+require 'spec_helper'
+
+module Omnibus
+  describe GitFetcher do
+    include_examples 'a software'
+
+    let(:remote)  { remote_git_repo('zlib') }
+    let(:version) { 'master' }
+
+    let(:source) do
+      { git: remote }
+    end
+
+    subject { described_class.new(software) }
+
+    describe '#fetch_required?' do
+      context 'when the repo is not cloned' do
+        it 'return true' do
+          expect(subject.fetch_required?).to be_truthy
+        end
+      end
+
+      context 'when the repo is cloned' do
+        before { subject.fetch }
+
+        context 'when the revisions are different' do
+          it 'return true' do
+            # Dirty the project_dir to differ the revisions
+            Dir.chdir(project_dir) do
+              FileUtils.touch("file-#{Time.now.to_i}")
+              shellout! %|git add .|
+              shellout! %|git commit -am "Add new file"|
+            end
+
+            expect(subject.fetch_required?).to be_truthy
+          end
+        end
+
+        context 'when the revisions are the same' do
+          it 'return false' do
+            expect(subject.fetch_required?).to be_falsey
+          end
+        end
+      end
+    end
+
+    describe '#version_guid' do
+      let(:revision) { '744d47c9152f7a06582d2076175bc60a80c9b169' }
+
+      it 'includes the current revision' do
+        expect(subject.version_guid).to eq("git:#{revision}")
+      end
+    end
+
+    describe '#clean' do
+      context 'when the project directory exists' do
+        before do
+          subject.fetch
+          create_file("#{project_dir}/file_a")
+          create_file("#{project_dir}/.file_b")
+        end
+
+        it 'cleans the git repo' do
+          subject.clean
+          expect("#{project_dir}/file_a").to_not be_a_file
+          expect("#{project_dir}/.file_b").to_not be_a_file
+        end
+
+        it 'returns true' do
+          expect(subject.clean).to be_truthy
+        end
+      end
+
+      context 'when the project directory does not exist' do
+        before do
+          remove_directory(project_dir)
+        end
+
+        it 'returns false' do
+          expect(subject.clean).to be_falsey
+        end
+      end
+    end
+
+    describe '#fetch'  do
+      let(:revision) { shellout!('git rev-parse HEAD', cwd: project_dir).stdout.strip }
+
+      it 'clones the repository' do
+        subject.fetch
+        expect("#{project_dir}/.git").to be_a_directory
+      end
+
+      context 'when the version is a tag' do
+        let(:version)  { 'v1.2.3' }
+        let(:remote)   { remote_git_repo('zlib', tags: [version]) }
+
+        it 'parses the tag' do
+          subject.fetch
+          expect(revision).to include('c02a264')
+        end
+      end
+
+      context 'when the version is a branch' do
+        let(:version) { 'sethvargo/magic_ponies' }
+        let(:remote)  { remote_git_repo('zlib', branches: [version]) }
+
+        it 'parses the branch' do
+          subject.fetch
+          expect(revision).to include('6ba1270')
+        end
+      end
+
+      context 'when the version is a ref' do
+        let(:version) { '68acfe1' }
+        let(:remote)  { remote_git_repo('zlib') }
+
+        it 'parses the ref' do
+          subject.fetch
+          expect(revision).to include('68acfe1')
+        end
+      end
+    end
+
+    describe '#version_for_cache' do
+      let(:revision) { shellout!('git rev-parse HEAD', cwd: project_dir).stdout.strip }
+
+      it 'includes the revision' do
+        expect(subject.version_for_cache).to eq("revision:#{revision}")
+      end
+    end
+  end
+end

spec/support/git_helpers.rb
@@ -1,0 +1,108 @@
+module Omnibus
+  module RSpec
+    module GitHelpers
+      include Util
+
+      def git_origin_for(repo, options = {})
+        "file://#{fake_git_remote("git://github.com/omnibus/#{repo}.git", options)}/.git"
+      end
+
+      def remote_git_repo(name, options = {})
+        path = File.join(remotes, name)
+        remote_url = "file://#{path}"
+
+        # Create a bogus software
+        FileUtils.mkdir_p(path)
+
+        Dir.chdir(path) do
+          git %|init --bare|
+          git %|config core.sharedrepository 1|
+          git %|config receive.denyNonFastforwards true|
+          git %|config receive.denyCurrentBranch ignore|
+        end
+
+        Dir.chdir(git_scratch) do
+          # Create a bogus configure file
+          File.open('configure', 'w') { |f| f.write('echo "Done!"') }
+
+          git %|init .|
+          git %|add .|
+          git %|commit -am "Initial commit for #{name}..."|
+          git %|remote add origin "#{remote_url}"|
+          git %|push origin master|
+
+          options[:tags].each do |tag|
+            File.open('tag', 'w') { |f| f.write(tag) }
+            git %|add tag|
+            git %|commit -am "Create tag #{tag}"|
+            git %|tag "#{tag}"|
+            git %|push origin "#{tag}"|
+          end if options[:tags]
+
+          options[:branches].each do |branch|
+            git %|checkout -b #{branch} master|
+            File.open('branch', 'w') { |f| f.write(branch) }
+            git %|add branch|
+            git %|commit -am "Create branch #{branch}"|
+            git %|push origin "#{branch}"|
+            git %|checkout master|
+          end if options[:branches]
+        end
+
+        path
+      end
+
+      # Calculate the git sha for the given ref.
+      #
+      # @param [#to_s] repo
+      #   the repository to show the ref for
+      # @param [#to_s] ref
+      #   the ref to show
+      #
+      # @return [String]
+      def sha_for_ref(repo, ref)
+        Dir.chdir(File.join(remotes, repo)) do
+          shellout! %|git show-ref #{ref}").stdout.split(/\s/).firs|
+        end
+      end
+
+      private
+
+      # The path to store the git remotes.
+      #
+      # @return [Pathname]
+      def remotes
+        path = File.join(tmp_path, 'git', 'remotes')
+        FileUtils.mkdir_p(path) unless File.directory?(path)
+        path
+      end
+
+      #
+      #
+      #
+      def git_scratch
+        path = File.join(tmp_path, 'git', 'scratch')
+        FileUtils.mkdir_p(path) unless File.directory?(path)
+        path
+      end
+
+      def git(command)
+        #
+        # We need to override some of the variable's git uses for generating
+        # the SHASUM for testing purposes
+        #
+        time = Time.at(680227200).strftime('%c %z')
+        env  = {
+          'GIT_AUTHOR_NAME'     => 'omnibus',
+          'GIT_AUTHOR_EMAIL'    => 'omnibus@getchef.com',
+          'GIT_AUTHOR_DATE'     => time,
+          'GIT_COMMITTER_NAME'  => 'omnibus',
+          'GIT_COMMITTER_EMAIL' => 'omnibus@gechef.com',
+          'GIT_COMMITTER_DATE'  => time,
+        }
+
+        shellout!("git #{command}", env: env)
+      end
+    end
+  end
+end

spec/unit/fetchers/git_fetcher_spec.rb
@@ -2,76 +2,147 @@ require 'spec_helper'
 
 module Omnibus
   describe GitFetcher do
+    let(:source_path) { '/local/path' }
+    let(:project_dir) { '/project/dir' }
+
     let(:software) do
       double(Software,
-        name: 'project',
-        source: { git: 'git@example.com:test/project.git' },
-        version: '0.0.1',
-        project_dir: '/tmp/project',
+        name: 'software',
+        source: { path: source_path },
+        project_dir: project_dir,
       )
     end
 
     subject { described_class.new(software) }
 
+    describe '#fetch_required?' do
+      context 'when the repository is not cloned' do
+        before { allow(subject).to receive(:cloned?).and_return(false) }
+
+        it 'returns true' do
+          expect(subject.fetch_required?).to be_truthy
+        end
+      end
+
+      context 'when the repository is cloned' do
+        before { allow(subject).to receive(:cloned?).and_return(true) }
+
+        context 'when the revision is difference' do
+          before { allow(subject).to receive(:same_revision?).and_return(false) }
+
+          it 'returns true' do
+            expect(subject.fetch_required?).to be_truthy
+          end
+        end
+
+        context 'when the same revision is checked out'  do
+          before { allow(subject).to receive(:same_revision?).and_return(true) }
+
+          it 'returns false' do
+            expect(subject.fetch_required?).to be_falsey
+          end
+        end
+      end
+    end
+
+    describe '#version_guid' do
+      let(:revision) { 'abcd1234' }
+
+      before do
+        allow(subject).to receive(:current_revision).and_return(revision)
+      end
+
+      it 'returns the revision' do
+        expect(subject.version_guid).to eq("git:#{revision}")
+      end
+    end
+
+    describe '#clean' do
+      before { allow(subject).to receive(:git) }
+
+      context 'when the repository is cloned' do
+        before do
+          allow(subject).to receive(:cloned?).and_return(true)
+        end
+
+        it 'gleans the directory' do
+          expect(subject).to receive(:git).with('clean -fdx')
+          subject.clean
+        end
+
+        it 'returns true' do
+          expect(subject.clean).to be_truthy
+        end
+      end
+
+      context 'when the repository is not cloned' do
+        before do
+          allow(subject).to receive(:cloned?).and_return(false)
+        end
+
+        it 'does not clean the repository' do
+          expect(subject).to_not receive(:git)
+          subject.clean
+        end
+
+        it 'returns false' do
+          expect(subject.clean).to be_falsey
+        end
+      end
+    end
+
     describe '#fetch' do
-      context 'when the project is cloned' do
-        before do
-          allow(subject).to receive(:existing_git_clone?).and_return(true)
-          allow(subject).to receive(:fetch_updates)
-        end
+      before do
+        allow(subject).to receive(:create_required_directories)
+        allow(subject).to receive(:git_fetch)
+        allow(subject).to receive(:git_clone)
+        allow(subject).to receive(:git_checkout)
+      end
 
-        context 'when the rev matches' do
-          before { allow(subject).to receive(:current_rev_matches_target_rev?).and_return(true) }
+      context 'when the repository is cloned' do
+        before { allow(subject).to receive(:cloned?).and_return(true) }
 
-          it 'does not fetch updates' do
-            expect(subject).to_not receive(:fetch_updates)
+        context 'when the revision is different' do
+          before { allow(subject).to receive(:same_revision?).and_return(false) }
+
+          it 'fetches and resets' do
+            expect(subject).to receive(:git_fetch)
             subject.fetch
           end
         end
 
-        context 'when the rev does not match' do
-          before { allow(subject).to receive(:current_rev_matches_target_rev?).and_return(false) }
+        context 'when the revision is the same' do
+          before { allow(subject).to receive(:same_revision?).and_return(true) }
 
-          it 'fetches the updates' do
-            expect(subject).to receive(:fetch_updates).once
+          it 'does not fetch or reset' do
+            expect(subject).to_not receive(:git_fetch)
             subject.fetch
           end
         end
       end
 
-      context 'when the project is not cloned' do
-        before do
-          allow(subject).to receive(:existing_git_clone?).and_return(false)
-          allow(subject).to receive(:clone)
-          allow(subject).to receive(:checkout)
-        end
+      context 'when the repository is not cloned' do
+        before { allow(subject).to receive(:cloned?).and_return(false) }
 
-        it 'clones the project' do
-          expect(subject).to receive(:clone).once
+        it 'clones the repository' do
+          expect(subject).to receive(:git_clone).once
           subject.fetch
         end
 
-        it 'checkouts the ref' do
-          expect(subject).to receive(:checkout).once
+        it 'checks out the correct revision' do
+          expect(subject).to receive(:git_checkout).once
           subject.fetch
         end
       end
+    end
 
-      context 'when something fails' do
-        let(:error_reporter) { double(Fetcher::ErrorReporter, explain: nil) }
+    describe '#version_for_cache' do
+      let(:revision) { 'abcd1234' }
 
-        before do
-          allow(subject).to receive(:existing_git_clone?).and_return(false)
-          allow(subject).to receive(:clone).and_raise(RuntimeError)
+      before { allow(subject).to receive(:current_revision).and_return(revision) }
 
-          allow(Fetcher::ErrorReporter).to receive(:new).and_return(error_reporter)
-        end
-
-        it 'retries 4 times' do
-          allow(subject).to receive(:sleep)
-          expect(subject).to receive(:clone).exactly(4).times
-          expect { subject.fetch }.to raise_error(RuntimeError)
-        end
+      it 'returns the shasum of the project_dir' do
+        expect(subject.version_for_cache).to eq("revision:#{revision}")
       end
     end
   end

commit 1ea32a0a007fd1b6110f8d7922f839e9c3b20589

Ensure `GitFetcher` properly resolves remote refs
This fixes a regression introduced in: 2f240e4

lib/omnibus/exceptions.rb
@@ -301,4 +301,12 @@ reported.
 EOH
     end
   end
+
+  class UnresolvableGitReference < Error
+    def initialize(ref)
+      super <<-EOH
+Could not resolve `#{ref}' to a valid git SHA-1.
+EOH
+    end
+  end
 end

lib/omnibus/fetchers/git_fetcher.rb
@@ -146,15 +146,11 @@ module Omnibus
     # @return [String]
     #
     def target_revision
-      @target_revision ||= git("rev-parse #{version}").stdout.strip
-    rescue CommandFailed => e
-      if e.message.include?('ambiguous argument')
-        @target_revision = git("rev-parse origin/#{version}").stdout.strip
-        @target_revision
-      else
-        log.warn { 'Could not determine target revision!' }
-        nil
-      end
+      @target_revision ||= if sha_hash?(version)
+                             version
+                           else
+                             revision_from_remote_reference(version)
+                           end
     end
 
     #
@@ -164,6 +160,76 @@ module Omnibus
     #
     def same_revision?
       current_revision == target_revision
+    end
+
+    #
+    # Determine if the given revision is a SHA
+    #
+    # @return [true, false]
+    #
+    def sha_hash?(rev)
+      rev =~ /^[0-9a-f]{4,40}$/
+    end
+
+    #
+    # Return the SHA corresponding to ref. If ref is an annotated tag,
+    # return the SHA that was tagged not the SHA of the tag itself.
+    #
+    # @return [String]
+    #
+    def revision_from_remote_reference(ref)
+      # execute `git ls-remote` the trailing '*' does globbing. This
+      # allows us to return the SHA of the tagged commit for annotated
+      # tags. We take care to only return exact matches in
+      # process_remote_list.
+      remote_list = git("ls-remote origin #{ref}*").stdout
+      commit_ref = dereference_annotated_tag(remote_list, ref)
+
+      unless commit_ref
+        raise UnresolvableGitReference.new(ref)
+      end
+      commit_ref
+    end
+
+    #
+    # Dereference annotated tags.
+    #
+    # The +remote_list+ parameter is assumed to look like this:
+    #
+    #   a2ed66c01f42514bcab77fd628149eccb4ecee28        refs/tags/rel-0.11.0
+    #   f915286abdbc1907878376cce9222ac0b08b12b8        refs/tags/rel-0.11.0^{}
+    #
+    # The SHA with ^{} is the commit pointed to by an annotated
+    # tag. If ref isn't an annotated tag, there will not be a line
+    # with trailing ^{}.
+    #
+    # @param [String] remote_list
+    #   output from `git ls-remote origin` command
+    # @param [String] ref
+    #   the target git ref
+    #
+    # @return [String]
+    #
+    def dereference_annotated_tag(remote_list, ref)
+      # We'll return the SHA corresponding to the ^{} which is the
+      # commit pointed to by an annotated tag. If no such commit
+      # exists (not an annotated tag) then we return the SHA of the
+      # ref.  If nothing matches, return "".
+      lines = remote_list.split("\n")
+      matches = lines.map { |line| line.split("\t") }
+      # First try for ^{} indicating the commit pointed to by an
+      # annotated tag.
+      tagged_commit = matches.find { |m| m[1].end_with?("#{ref}^{}") }
+      if tagged_commit
+        tagged_commit.first
+      else
+        found = matches.find { |m| m[1].end_with?("#{ref}") }
+        if found
+          found.first
+        else
+          nil
+        end
+      end
     end
 
     #

spec/functional/fetchers/git_fetcher_spec.rb
@@ -98,6 +98,16 @@ module Omnibus
         end
       end
 
+      context 'when the version is an annnotated tag' do
+        let(:version)  { 'v1.2.4' }
+        let(:remote)   { remote_git_repo('zlib', annotated_tags: [version]) }
+
+        it 'it defererences and parses the annotated tag' do
+          subject.fetch
+          expect(revision).to eq('efde208366abd0f91419d8a54b45e3f6e0540105')
+        end
+      end
+
       context 'when the version is a branch' do
         let(:version) { 'sethvargo/magic_ponies' }
         let(:remote)  { remote_git_repo('zlib', branches: [version]) }
@@ -108,13 +118,32 @@ module Omnibus
         end
       end
 
-      context 'when the version is a ref' do
+      context 'when the version is a full SHA-1' do
+        let(:version) { '45ded6d3b1a35d66ed866b2c3eb418426e6382b0' }
+        let(:remote)  { remote_git_repo('zlib') }
+
+        it 'parses the full SHA-1' do
+          subject.fetch
+          expect(revision).to eq('45ded6d3b1a35d66ed866b2c3eb418426e6382b0')
+        end
+      end
+
+      context 'when the version is a abbreviated SHA-1' do
         let(:version) { '45ded6d' }
         let(:remote)  { remote_git_repo('zlib') }
 
-        it 'parses the ref' do
+        it 'parses the abbreviated SHA-1' do
           subject.fetch
           expect(revision).to eq('45ded6d3b1a35d66ed866b2c3eb418426e6382b0')
+        end
+      end
+
+      context 'when the version is a non-existent ref' do
+        let(:version) { 'fufufufufu' }
+        let(:remote)  { remote_git_repo('zlib') }
+
+        it 'raise an exception' do
+          expect { subject.fetch }.to raise_error(UnresolvableGitReference)
         end
       end
     end

spec/support/git_helpers.rb
@@ -33,6 +33,14 @@ module Omnibus
           git %|remote add origin "#{remote_url}"|
           git %|push origin master|
 
+          options[:annotated_tags].each do |tag|
+            File.open('tag', 'w') { |f| f.write(tag) }
+            git %|add tag|
+            git %|commit -am "Create tag #{tag}"|
+            git %|tag "#{tag}" -m "#{tag}"|
+            git %|push origin "#{tag}"|
+          end if options[:annotated_tags]
+
           options[:tags].each do |tag|
             File.open('tag', 'w') { |f| f.write(tag) }
             git %|add tag|