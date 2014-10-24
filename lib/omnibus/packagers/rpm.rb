87f10bf033505be4d8e0b8eb43b0e76beced20bd
Leigh Taylor <barnabear@users.noreply.github.com>
2014-10-24 23:10:58 +0100

    Update rpm.rb
    If platform is 'pidora' then safe_architecture returns 'armv6hl'

--- lib/omnibus/packagers/rpm.rb
+++ lib/omnibus/packagers/rpm.rb
@@ -449,7 +449,11 @@ module Omnibus
     # @return [String]
     #
     def safe_architecture
-      Ohai['kernel']['machine']
+      if Ohai['platform'] == 'pidora'
+        'armv6hl'
+      else
+        Ohai['kernel']['machine']
+      end
     end
   end
 end
