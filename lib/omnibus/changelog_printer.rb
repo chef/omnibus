5e23e3bae5850607e9fc867e474d7d2df90c5041
Steven Danna <steve@opscode.com>
2015-03-04 15:04:35 +0000

    Print changelog of updated components if the repository is on disk

--- lib/omnibus/changelog_printer.rb
+++ lib/omnibus/changelog_printer.rb
@@ -33,6 +33,7 @@ module Omnibus
     end
 
     def print_new_components
+      return if diff.added.empty?
       puts "New Components"
       diff.added.each do |entry|
         puts "* #{entry[:name]} (#{entry[:new_version]})"
@@ -41,21 +42,27 @@ module Omnibus
     end
 
     def print_updated_components
+      return if diff.updated.empty?
       puts "Updated Components"
       diff.updated.each do |entry|
-        puts "* #{entry[:name]} (#{entry[:old_version]} -> #{entry[:new_version]})"
+        puts sprintf("* %s (%.8s -> %.8s)",
+                     entry[:name], entry[:old_version], entry[:new_version])
+        if entry[:source_type] == 'git' && ::File.directory?("../#{entry[:name]}/.git")
+          cl = ChangeLog.new(entry[:old_version], entry[:new_version], GitRepository.new("../#{entry[:name]}"))
+          print_changelog(cl, 2)
+        end
       end
       puts ""
     end
 
     def print_removed_components
+      return if diff.removed.empty?
       puts "Removed Components"
       diff.removed.each do |entry|
         puts "* #{entry[:name]} (#{entry[:old_version]})"
       end
       puts ""
     end
-
 
     def print_contributors
       puts "### Contributors\n"
