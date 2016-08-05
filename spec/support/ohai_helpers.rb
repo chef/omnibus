0dd040c52ae1794cb03cd1c20d39ecfeeec5c74d
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2016-08-05 15:22:22 -0400

    Stub out a few windows test paths

--- spec/support/ohai_helpers.rb
+++ spec/support/ohai_helpers.rb
@@ -11,10 +11,18 @@ module Omnibus
         ohai = Mash.from_hash(Fauxhai.mock(options, &block).data)
         allow(Ohai).to receive(:ohai).and_return(ohai)
 
-        # If we asked for Windows, we should also specify that magical
-        # +File::ALT_SEPARATOR+ variable
         if options[:platform] && options[:platform] == "windows"
+          # If we asked for Windows, we should also specify that magical
+          # +File::ALT_SEPARATOR+ variable
           stub_const("File::ALT_SEPARATOR", '\\')
+          # And don't actually perform expand_path because otherwise,
+          # C:/foo will turn into "/Users/foo/bar/baz/C:/foo" on non-windows
+          # and /foo will turn into "C:/foo" on windows.
+          # Since most paths in our tests are unixy-paths, we'll turn off
+          # expand_path just on windows by default.
+          if windows? && options[:enable_expand_path] != false
+            allow(File).to receive(:expand_path) { |arg| arg }
+          end
         end
       end
     end
