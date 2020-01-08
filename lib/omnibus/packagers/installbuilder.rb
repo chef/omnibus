3c07cc2760303b6f7b8c6a4a03ef5a116968d318
Fries <dfries@ptc.com>
2020-01-08 10:50:20 +0200

    DF - updating copyright to PTC's based
    Base on feedback on pull request:
     Add Installbuilder packager(installbuilder) #908
    
    From Tim Smith
    
    Signed-off-by: Fries <dfries@ptc.com>

--- lib/omnibus/packagers/installbuilder.rb
+++ lib/omnibus/packagers/installbuilder.rb
@@ -1,5 +1,5 @@
 #
-# Copyright 2014-2019 Chef Software, Inc.
+# 2020 © PTC Inc.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
