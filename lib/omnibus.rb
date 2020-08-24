48d99b4a88d7dd57a6a6d05ece22fb2be6498ac4
Tim Smith <tsmith84@gmail.com>
2020-08-24 10:21:00 -0700

    Switch expand_path on file to expand_path on dir
    Remove a .. here
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/omnibus.rb
+++ lib/omnibus.rb
@@ -208,7 +208,7 @@ module Omnibus
     # @return [Pathname]
     #
     def source_root
-      @source_root ||= Pathname.new(File.expand_path("../..", __FILE__))
+      @source_root ||= Pathname.new(File.expand_path("..", __dir__))
     end
 
     #
