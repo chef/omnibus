195b5d6a4791fd4d1e4e415f9de945dc118c25a1
Tim Smith <tsmith84@gmail.com>
2021-02-10 21:06:29 -0800

    Switch from -02 to -03 on Linux/macOS
    We're already doing -O3 on Windows
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/omnibus/software.rb
+++ lib/omnibus/software.rb
@@ -739,7 +739,7 @@ module Omnibus
         else
           {
             "LDFLAGS" => "-Wl,-rpath,#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib",
-            "CFLAGS" => "-I#{install_dir}/embedded/include -O2 -D_FORTIFY_SOURCE=2 -fstack-protector",
+            "CFLAGS" => "-I#{install_dir}/embedded/include -O3 -D_FORTIFY_SOURCE=2 -fstack-protector",
           }
         end
 
