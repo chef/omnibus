6f4ec8f9d05552bbf1b259dcf740867a912bacad
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2021-11-05 10:42:50 -0400

    [licensing] ship licensing text for third-party licenses too (#123)
    * [license] pull the license for identified third party project licenses
    
    * [license] handle unknown standard library.
    
    * Apply suggestions from code review
    
    Co-authored-by: Kylian Serrania <kylian.serrania@datadoghq.com>
    
    Co-authored-by: Kylian Serrania <kylian.serrania@datadoghq.com>

--- lib/omnibus/licensing.rb
+++ lib/omnibus/licensing.rb
@@ -61,6 +61,7 @@ module Omnibus
 
           license_collector.process_transitive_dependency_licensing_info
           license_collector.create_project_license_file
+          license_collector.third_party_licenses_collector
           license_collector.raise_if_warnings_fatal!
         end
       end
@@ -169,7 +170,7 @@ module Omnibus
       end
 
       # Check used license is a standard license
-      if project.license != "Unspecified" && !STANDARD_LICENSES.include?(project.license)
+      if project.license != "Unspecified" && !STANDARD_LICENSES.keys.include?(project.license)
         licensing_info("Project '#{project.name}' is using '#{project.license}' which is not one of the standard licenses identified in https://opensource.org/licenses/alphabetical. Consider using one of the standard licenses.")
       end
 
@@ -186,7 +187,7 @@ module Omnibus
         end
 
         # Check if the software license is one of the standard licenses
-        if license_info[:license] != "Unspecified" && !STANDARD_LICENSES.include?(license_info[:license])
+        if license_info[:license] != "Unspecified" && !STANDARD_LICENSES.keys.include?(license_info[:license])
           licensing_info("Software '#{software_name}' uses license '#{license_info[:license]}' which is not one of the standard licenses identified in https://opensource.org/licenses/alphabetical. Consider using one of the standard licenses.")
         end
       end
@@ -393,6 +394,47 @@ module Omnibus
         end
 
         map
+      end
+    end
+
+    #
+    # Function that pulls the third party licenses defined for the project, then fetches
+    # them and drops them into the project output directory.
+    #
+    def third_party_licenses_collector
+      licenses = {}
+
+      if project.third_party_licenses && project.third_party_licenses != "Unspecified"
+        license_table = CSV.parse(File.read(project.third_party_licenses), headers: true)
+        license_table.each do |lic|
+          unless licenses.key?(lic["License"])
+            licenses[lic["License"]] = STANDARD_LICENSES[lic["License"]]
+          end
+        end
+
+        licenses.each do |name, url|
+          if url.nil?
+            licensing_warning("Unknown standard license for software '#{name}'.")
+            next
+          end
+          output_file = license_package_location("THIRD-PARTY", name)
+          begin
+            download_file!(url, output_file, enable_progress_bar: false)
+            File.chmod 0644, output_file unless windows?
+          rescue SocketError,
+                 Errno::ECONNREFUSED,
+                 Errno::ECONNRESET,
+                 Errno::ENETUNREACH,
+                 Timeout::Error,
+                 OpenURI::HTTPError,
+                 OpenSSL::SSL::SSLError
+            licensing_warning("Can not download license file '#{url}' for software '#{software_name}'.")
+            # If we got here, we need to fail now so we don't take a git
+            # cache snapshot, or else the software build could be restored
+            # from cache without fixing the license issue.
+            raise_if_warnings_fatal!
+          end
+        end
       end
     end
 
@@ -650,98 +692,99 @@ EOH
       end
     end
 
-    STANDARD_LICENSES = [
+    STANDARD_LICENSES = {
       #
       # Below licenses are compiled based on https://opensource.org/licenses/alphabetical
       #
-      "AFL-3.0",       # Academic Free License 3.0
-      "AGPL-3.0",      # Affero General Public License
-      "APL-1.0",       # Adaptive Public License
-      "Apache-2.0",    # Apache License 2.0
-      "APSL-2.0",      # Apple Public Source License
-      "Artistic-2.0",  # Artistic license 2.0
-      "AAL",           # Attribution Assurance Licenses
-      "BSD-3-Clause",  # BSD 3-Clause "New" or "Revised" License
-      "BSD-2-Clause",  # BSD 2-Clause "Simplified" or "FreeBSD" License
-      "BSL-1.0",       # Boost Software License
-      "CECILL-2.1",    # CeCILL License 2.1
-      "CATOSL-1.1",    # Computer Associates Trusted Open Source License 1.1
-      "CDDL-1.0",      # Common Development and Distribution License 1.0
-      "CPAL-1.0",      # Common Public Attribution License 1.0
-      "CUA-OPL-1.0",   # CUA Office Public License Version 1.0
-      "EUDatagrid",    # EU DataGrid Software License
-      "EPL-1.0",       # Eclipse Public License 1.0
-      "eCos-2.0",      # eCos License version 2.0
-      "ECL-2.0",       # Educational Community License, Version 2.0
-      "EFL-2.0",       # Eiffel Forum License V2.0
-      "Entessa",       # Entessa Public License
-      "EUPL-1.1",      # European Union Public License, Version 1.1
-      "Fair",          # Fair License
-      "Frameworx-1.0", # Frameworx License
-      "FPL-1.0.0",     # Free Public License 1.0.0
-      "AGPL-3.0",      # GNU Affero General Public License v3
-      "GPL-2.0",       # GNU General Public License version 2.0
-      "GPL-3.0",       # GNU General Public License version 3.0
-      "LGPL-2.1",      # GNU Library or "Lesser" General Public License version 2.1
-      "LGPL-3.0",      # GNU Library or "Lesser" General Public License version 3.0
-      "HPND",          # Historical Permission Notice and Disclaimer
-      "IPL-1.0",       # IBM Public License 1.0
-      "IPA",           # IPA Font License
-      "ISC",           # ISC License
-      "LPPL-1.3c",     # LaTeX Project Public License 1.3c
-      "LiLiQ-P",       # Licence Libre du Quebec Permissive
-      "LiLiQ-R",       # Licence Libre du Quebec Reciprocite
-      "LiLiQ-R+",      # Licence Libre du Quebec Reciprocite forte
-      "LPL-1.02",      # Lucent Public License Version 1.02
-      "MirOS",         # MirOS Licence
-      "MS-PL",         # Microsoft Public License
-      "MS-RL",         # Microsoft Reciprocal License
-      "MIT",           # MIT license
-      "Motosoto",      # Motosoto License
-      "MPL-2.0",       # Mozilla Public License 2.0
-      "Multics",       # Multics License
-      "NASA-1.3",      # NASA Open Source Agreement 1.3
-      "NTP",           # NTP License
-      "Naumen",        # Naumen Public License
-      "NGPL",          # Nethack General Public License
-      "Nokia",         # Nokia Open Source License
-      "NPOSL-3.0",     # Non-Profit Open Software License 3.0
-      "OCLC-2.0",      # OCLC Research Public License 2.0
-      "OGTSL",         # Open Group Test Suite License
-      "OSL-3.0",       # Open Software License 3.0
-      "OPL-2.1",       # OSET Public License version 2.1
-      "PHP-3.0",       # PHP License 3.0
-      "PostgreSQL",    # The PostgreSQL License
-      "Python-2.0",    # Python License
-      "CNRI-Python",   # CNRI Python license
-      "QPL-1.0",       # Q Public License
-      "RPSL-1.0",      # RealNetworks Public Source License V1.0
-      "RPL-1.5",       # Reciprocal Public License 1.5
-      "RSCPL",         # Ricoh Source Code Public License
-      "OFL-1.1",       # SIL Open Font License 1.1
-      "SimPL-2.0",     # Simple Public License 2.0
-      "Sleepycat",     # Sleepycat License
-      "SPL-1.0",       # Sun Public License 1.0
-      "Watcom-1.0",    # Sybase Open Watcom Public License 1.0
-      "NCSA",          # University of Illinois/NCSA Open Source License
-      "UPL",           # Universal Permissive License
-      "VSL-1.0",       # Vovida Software License v. 1.0
-      "W3C",           # W3C License
-      "WXwindows",     # wxWindows Library License
-      "Xnet",          # X.Net License
-      "0BSD",          # Zero Clause BSD License
-      "ZPL-2.0",       # Zope Public License 2.0
-      "Zlib",          # zlib/libpng license
+      "AFL-3.0" => "https://opensource.org/licenses/AFL-3.0",             # Academic Free License 3.0
+      "AGPL-3.0" => "https://opensource.org/licenses/AGPL-3.0",           # Affero General Public License
+      "APL-1.0" => "https://opensource.org/licenses/APL-1.0",             # Adaptive Public License
+      "Apache-2.0" => "https://opensource.org/licenses/Apache-2.0",       # Apache License 2.0
+      "APSL-2.0" => "https://opensource.org/licenses/APSL-2.0",           # Apple Public Source License
+      "Artistic-2.0" => "https://opensource.org/licenses/Artistic-2.0",   # Artistic license 2.0
+      "AAL" => "https://opensource.org/licenses/AALAAL",                  # Attribution Assurance Licenses
+      "BSD-3-Clause" => "https://opensource.org/licenses/BSD-3-Clause",   # BSD 3-Clause "New" or "Revised" License
+      "BSD-2-Clause" => "https://opensource.org/licenses/BSD-2-Clause",   # BSD 2-Clause "Simplified" or "FreeBSD" License
+      "BSD-1-Clause" => "https://opensource.org/licenses/BSD-1-Clause",   # BSD 1-Clause
+      "BSL-1.0" => "https://opensource.org/licenses/BSL-1.0",             # Boost Software License
+      "CECILL-2.1" => "https://opensource.org/licenses/CECILL-2.1",       # CeCILL License 2.1
+      "CATOSL-1.1" => "https://opensource.org/licenses/CATOSL-1.1",       # Computer Associates Trusted Open Source License 1.1
+      "CDDL-1.0" => "https://opensource.org/licenses/CDDL-1.0",           # Common Development and Distribution License 1.0
+      "CPAL-1.0" => "https://opensource.org/licenses/CPAL-1.0",           # Common Public Attribution License 1.0
+      "CUA-OPL-1.0" => "https://opensource.org/licenses/CUA-OPL-1.0",     # CUA Office Public License Version 1.0
+      "EUDatagrid" => "https://opensource.org/licenses/EUDatagrid",       # EU DataGrid Software License
+      "EPL-1.0" => "https://opensource.org/licenses/EPL-1.0",             # Eclipse Public License 1.0
+      "eCos-2.0" => "https://opensource.org/licenses/eCos-2.0",           # eCos License version 2.0
+      "ECL-2.0" => "https://opensource.org/licenses/ECL-2.0",             # Educational Community License,            Version 2.0
+      "EFL-2.0" => "https://opensource.org/licenses/EFL-2.0",             # Eiffel Forum License V2.0
+      "Entessa" => "https://opensource.org/licenses/Entessa",             # Entessa Public License
+      "EUPL-1.1" => "https://opensource.org/licenses/EUPL-1.1",           # European Union Public License,            Version 1.1
+      "Fair" => "https://opensource.org/licenses/Fair",                   # Fair License
+      "Frameworx-1.0" => "https://opensource.org/licenses/Frameworx-1.0", # Frameworx License
+      "FPL-1.0.0" => "https://opensource.org/licenses/FPL-1.0.0",         # Free Public License 1.0.0
+      "AGPL-3.0" => "https://opensource.org/licenses/AGPL-3.0",           # GNU Affero General Public License v3
+      "GPL-2.0" => "https://opensource.org/licenses/GPL-2.0",             # GNU General Public License version 2.0
+      "GPL-3.0" => "https://opensource.org/licenses/GPL-3.0",             # GNU General Public License version 3.0
+      "LGPL-2.1" => "https://opensource.org/licenses/LGPL-2.1",           # GNU Library or "Lesser" General Public License version 2.1
+      "LGPL-3.0" => "https://opensource.org/licenses/LGPL-3.0",           # GNU Library or "Lesser" General Public License version 3.0
+      "HPND" => "https://opensource.org/licenses/HPND",                   # Historical Permission Notice and Disclaimer
+      "IPL-1.0" => "https://opensource.org/licenses/IPL-1.0",             # IBM Public License 1.0
+      "IPA" => "https://opensource.org/licenses/IPA",                     # IPA Font License
+      "ISC" => "https://opensource.org/licenses/ISC",                     # ISC License
+      "LPPL-1.3c" => "https://opensource.org/licenses/LPPL-1.3c",         # LaTeX Project Public License 1.3c
+      "LiLiQ-P" => "https://opensource.org/licenses/LiLiQ-P",             # Licence Libre du Quebec Permissive
+      "LiLiQ-R" => "https://opensource.org/licenses/LiLiQ-R",             # Licence Libre du Quebec Reciprocite
+      "LiLiQ-R+" => "https://opensource.org/licenses/LiLiQ-R+",           # Licence Libre du Quebec Reciprocite forte
+      "LPL-1.02" => "https://opensource.org/licenses/LPL-1.02",           # Lucent Public License Version 1.02
+      "MirOS" => "https://opensource.org/licenses/MirOS",                 # MirOS Licence
+      "MS-PL" => "https://opensource.org/licenses/MS-PL",                 # Microsoft Public License
+      "MS-RL" => "https://opensource.org/licenses/MS-RL",                 # Microsoft Reciprocal License
+      "MIT" => "https://opensource.org/licenses/MIT",                     # MIT license
+      "Motosoto" => "https://opensource.org/licenses/Motosoto",           # Motosoto License
+      "MPL-2.0" => "https://opensource.org/licenses/MPL-2.0",             # Mozilla Public License 2.0
+      "Multics" => "https://opensource.org/licenses/Multics",             # Multics License
+      "NASA-1.3" => "https://opensource.org/licenses/NASA-1.3",           # NASA Open Source Agreement 1.3
+      "NTP" => "https://opensource.org/licenses/NTP",                     # NTP License
+      "Naumen" => "https://opensource.org/licenses/Naumen",               # Naumen Public License
+      "NGPL" => "https://opensource.org/licenses/NGPL",                   # Nethack General Public License
+      "Nokia" => "https://opensource.org/licenses/Nokia",                 # Nokia Open Source License
+      "NPOSL-3.0" => "https://opensource.org/licenses/NPOSL-3.0",         # Non-Profit Open Software License 3.0
+      "OCLC-2.0" => "https://opensource.org/licenses/OCLC-2.0",           # OCLC Research Public License 2.0
+      "OGTSL" => "https://opensource.org/licenses/OGTSL",                 # Open Group Test Suite License
+      "OSL-3.0" => "https://opensource.org/licenses/OSL-3.0",             # Open Software License 3.0
+      "OPL-2.1" => "https://opensource.org/licenses/OPL-2.1",             # OSET Public License version 2.1
+      "PHP-3.0" => "https://opensource.org/licenses/PHP-3.0",             # PHP License 3.0
+      "PostgreSQL" => "https://opensource.org/licenses/PostgreSQL",       # The PostgreSQL License
+      "Python-2.0" => "https://opensource.org/licenses/Python-2.0",       # Python License
+      "CNRI-Python" => "https://opensource.org/licenses/CNRI-Python",     # CNRI Python license
+      "QPL-1.0" => "https://opensource.org/licenses/QPL-1.0",             # Q Public License
+      "RPSL-1.0" => "https://opensource.org/licenses/RPSL-1.0",           # RealNetworks Public Source License V1.0
+      "RPL-1.5" => "https://opensource.org/licenses/RPL-1.5",             # Reciprocal Public License 1.5
+      "RSCPL" => "https://opensource.org/licenses/RSCPL",                 # Ricoh Source Code Public License
+      "OFL-1.1" => "https://opensource.org/licenses/OFL-1.1",             # SIL Open Font License 1.1
+      "SimPL-2.0" => "https://opensource.org/licenses/SimPL-2.0",         # Simple Public License 2.0
+      "Sleepycat" => "https://opensource.org/licenses/Sleepycat",         # Sleepycat License
+      "SPL-1.0" => "https://opensource.org/licenses/SPL-1.0",             # Sun Public License 1.0
+      "Watcom-1.0" => "https://opensource.org/licenses/Watcom-1.0",       # Sybase Open Watcom Public License 1.0
+      "NCSA" => "https://opensource.org/licenses/NCSA",                   # University of Illinois/NCSA Open Source License
+      "UPL" => "https://opensource.org/licenses/UPL",                     # Universal Permissive License
+      "VSL-1.0" => "https://opensource.org/licenses/VSL-1.0",             # Vovida Software License v. 1.0
+      "W3C" => "https://opensource.org/licenses/W3C",                     # W3C License
+      "WXwindows" => "https://opensource.org/licenses/WXwindows",         # wxWindows Library License
+      "Xnet" => "https://opensource.org/licenses/Xnet",                   # X.Net License
+      "0BSD" => "https://opensource.org/licenses/0BSD",                   # Zero Clause BSD License
+      "ZPL-2.0" => "https://opensource.org/licenses/ZPL-2.0",             # Zope Public License 2.0
+      "Zlib" => "https://opensource.org/licenses/Zlib",                   # zlib/libpng license
       #
       # In addition to these we would like to add some of the licenses that
       # are frequently used in our depedencies.
       #
-      "Public-Domain", # https://opensource.org/faq#public-domain
-      "Ruby",          # http://www.ruby-lang.org/en/LICENSE.txt
-      "Erlang-Public", # http://www.erlang.org/EPLICENSE
-      "Oracle-Binary", # http://www.oracle.com/technetwork/java/javase/terms/license/index.html
-      "OpenSSL",       # https://www.openssl.org/source/license.html
-      "Chef-MLSA",     # https://www.chef.io/online-master-agreement/
-    ].freeze
+      "Public-Domain" => "https://opensource.org/faq#public-domain",
+      "Ruby" => "http://www.ruby-lang.org/en/LICENSE.txt",
+      "Erlang-Public" => "http://www.erlang.org/EPLICENSE",
+      "Oracle-Binary" => "http://www.oracle.com/technetwork/java/javase/terms/license/index.html",
+      "OpenSSL" => "https://www.openssl.org/source/license.html",
+      "Chef-MLSA" => "https://www.chef.io/online-master-agreement/"
+    }.freeze
   end
 end
