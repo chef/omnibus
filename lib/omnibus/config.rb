c943ac599f1afcc89edbc9b02fa38b5ffe5b5897
Kanad Kumar <kankumar@progress.com>
2022-11-14 21:01:45 +0530

    use_internal_sources to true
    Signed-off-by: Kanad Kumar <kankumar@progress.com>

--- lib/omnibus/config.rb
+++ lib/omnibus/config.rb
@@ -611,7 +611,7 @@ module Omnibus
     # omnibus package using artifactory as the source url.
     #
     # @return [true, false]
-    default(:use_internal_sources, false)
+    default(:use_internal_sources, true)
 
     # --------------------------------------------------
     # @!endgroup
