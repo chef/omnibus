9ec2b767d5e613450c5d068ae3ccaf1c19f13e88
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2024-01-03 10:50:17 +0100

    Add compression related options for deb & rpm packagers [APL-2626] (#163)
    * Use xz level 9 compression for packages
    
    * packagers: deb: allow compression level & nb threads to be specified
    
    * packagers: rpm: allow compression level & nb threads to be provided
    
    * please rubocop
    
    * use the same default for both deb & rpm
    
    * fix error messages
    
    * fix inverted logic
    
    * allow compressin algorithm to be passed
    
    * fix inverted logic
    
    * fix typo
    
    * fix default rpm compression algo
    
    * fix incorrect assignments
    
    * generalize comment
    
    ---------
    
    Co-authored-by: Slavek Kabrda <slavek.kabrda@datadoghq.com>

--- lib/omnibus/packagers/rpm.rb
+++ lib/omnibus/packagers/rpm.rb
@@ -454,6 +454,9 @@ module Omnibus
                         files: files,
                         build_dir: build_dir(debug),
                         platform_family: Ohai["platform_family"],
+                        compression_level: compression_level,
+                        compression_threads: compression_threads,
+                        compression_algo: compression_algo,
                       })
     end
 
@@ -830,5 +833,40 @@ module Omnibus
         shellout!("yum -y remove #{packages}")
       end
     end
+
+    def compression_level(val = nil)
+      unless val.nil?
+        unless val >= 0 && val <= 9
+          raise InvalidValue.new(:compression_level, 'be an Integer between 0 and 9 included')
+        end
+      end
+      @compression_level = val || 6
+    end
+    expose :compression_level
+
+    def compression_threads(val = nil)
+      unless val.nil?
+        unless val > 0 && val < 32
+          raise InvalidValue.new(:compression_threads, 'be a stricly positive and lower than 32 Integer')
+        end
+      end
+      @compression_threads = val || 1
+    end
+    expose :compression_threads
+
+    def compression_algo(val = nil)
+      unless val.nil?
+        if val == "xz"
+          val = "xzdio"
+        elsif val == "gzip"
+          val = "gzdio"
+        else
+          raise InvalidValue.new(:compression_algo, 'be one of xz or gzip')
+        end
+
+      end
+      @compression_algo = val || "gzdio"
+    end
+    expose :compression_algo
   end
 end
