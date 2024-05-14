68518f0be4cdf4f946dd47b69a26ffb5d17c1eee
poorndm <poorndm@progress.com>
2024-05-14 15:52:22 +0530

    add debugging stmt for signing script
    Signed-off-by: poorndm <poorndm@progress.com>

--- lib/omnibus/packagers/rpm.rb
+++ lib/omnibus/packagers/rpm.rb
@@ -452,7 +452,9 @@ module Omnibus
           if dist_tag != ".el8" && dist_tag != ".el9" && dist_tag != ".amazon2023"
             sign_cmd.prepend("#{signing_script} \"").concat("\"")
             log.info(log_key) { " DEBUGGING Stmt  - omnibus-rpm.rb RHEL 8 and Amazon-2023 has gpg-agent running so skipping the expect script -sign_cmd -  #{sign_cmd} AND signing_script -> #{signing_script}" }
-            echo "<<<<<< signing_script - `cat #{signing_script}` "
+            log.info(log_key) { "<<<<<< signing_script - cat #{signing_script} " }
+            command1="cat #{signing_script} "
+            shellout!("#{command1}")
           end
 
           shellout!("#{sign_cmd}", environment: { "HOME" => home })
