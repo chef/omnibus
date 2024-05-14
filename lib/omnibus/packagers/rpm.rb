6c8a8a434696af787b9b87261c9c35f041c88ff0
poorndm <poorndm@progress.com>
2024-05-14 15:56:36 +0530

    add debugging stmt for signing script - fix syntax
    Signed-off-by: poorndm <poorndm@progress.com>

--- lib/omnibus/packagers/rpm.rb
+++ lib/omnibus/packagers/rpm.rb
@@ -453,7 +453,7 @@ module Omnibus
             sign_cmd.prepend("#{signing_script} \"").concat("\"")
             log.info(log_key) { " DEBUGGING Stmt  - omnibus-rpm.rb RHEL 8 and Amazon-2023 has gpg-agent running so skipping the expect script -sign_cmd -  #{sign_cmd} AND signing_script -> #{signing_script}" }
             log.info(log_key) { "<<<<<< signing_script - cat #{signing_script} " }
-            command1="cat #{signing_script} "
+            command1 = "cat #{signing_script} "
             shellout!("#{command1}")
           end
 
