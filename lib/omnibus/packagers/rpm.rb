0c2ed20e886c7af400ba1403d5a16f20b13475bb
poorndm <poorndm@progress.com>
2024-07-02 16:03:18 +0530

    rpm-signing - test-keys
    Signed-off-by: poorndm <poorndm@progress.com>

--- lib/omnibus/packagers/rpm.rb
+++ lib/omnibus/packagers/rpm.rb
@@ -440,7 +440,27 @@ module Omnibus
               gpg_name: "Opscode Packages",
               gpg_path: "#{ENV["HOME"]}/.gnupg", # TODO: Make this configurable
             })
-        end
+          end
+      INSTALL_USER_NAME=chef-ci,
+      PACKAGE_SIGNING_KEY=`cat /home/chef-ci/private_key`
+      #PACKAGE_SIGNING_KEY_UNIQUE_ID=${vault("account/static/packages/test_keys", "packages_at_chef_io_unique_id")},
+      PACKAGE_SIGNING_KEY_UNIQUE_ID="E3531A01"
+      PACKAGE_SIGNING_KEY="${PACKAGE_SIGNING_KEY:?PACKAGE_SIGNING_KEY must be set}"
+      PACKAGE_SIGNING_KEY_UNIQUE_ID="${PACKAGE_SIGNING_KEY_UNIQUE_ID:?PACKAGE_SIGNING_KEY_UNIQUE_ID must be set}"
+
+      echo "PACKAGE_SIGNING_KEY= $PACKAGE_SIGNING_KEY"
+      echo "PACKAGE_SIGNING_KEY_UNIQUE_I = $PACKAGE_SIGNING_KEY_UNIQUE_ID"
+
+      echo "--- Importing packages@chef.io gpg signing key"
+      if ! gpg --list-secret-keys "$PACKAGE_SIGNING_KEY_UNIQUE_ID"; then
+        echo "$PACKAGE_SIGNING_KEY" | gpg --import
+      fi
+
+      echo "--- Installing .rpmmacros"
+      cat <<-EOF > ~/.rpmmacros
+         %_signature gpg
+         %_gpg_name  Opscode Packages
+      EOF
 
         sign_cmd = "rpmsign --addsign #{rpm_file}"
         log.info(log_key) { " DEBUGGING Stmt  - omnibus-rpm.rb Sign_cmd -#{sign_cmd} - rpm file - #{rpm_file}" }
