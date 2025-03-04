cdeaa614c48e60368a0b3bfa9cd737cce5b37cbe
muthuja <muthujabavaji.vempalli@progress.com>
2025-03-04 20:09:41 +0530

    export path to homebrew
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

--- lib/omnibus/fetchers/net_fetcher.rb
+++ lib/omnibus/fetchers/net_fetcher.rb
@@ -163,8 +163,9 @@ def execute_tests(file_path)
   log.info(log_key) { "-----DEBUG-----checking gtar and xz versions and path" }
   shellout!("gtar --version")
   shellout!("which gtar")
-  shellout!("xz --version")
+  shellout!(export PATH="/opt/homebrew/bin/xz:$PATH")
   shellout!("which xz")
+  shellout!("xz --version")
   log.info(log_key) { "-----DEBUG-----end of the test to here" }
   # Extract the archive
   archive_path = File.join(test_dir, File.basename(file_path))
