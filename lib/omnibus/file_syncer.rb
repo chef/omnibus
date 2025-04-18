7fbd2effc5a4308e48529d2d2dab38bd58b76ace
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2025-04-18 16:59:48 +0200

    Revert "file_syncer: force link creation (#231)" (#232)
    This reverts commit cb2c9de1416444d46e26d432288c5375d970d940.

--- lib/omnibus/file_syncer.rb
+++ lib/omnibus/file_syncer.rb
@@ -148,7 +148,7 @@ module Omnibus
           target = File.readlink(source_file)
 
           Dir.chdir(destination) do
-            FileUtils.ln_sf(target, "#{destination}/#{relative_path}", force: true)
+            FileUtils.ln_sf(target, "#{destination}/#{relative_path}")
           end
         when :file
           source_stat = File.stat(source_file)
